7e9d1fc6d5d69fe2af784d1a8169938af17f1893
Lucas Barcellos <lucas.t.barcellos@gmail.com>
2020-07-21 02:40:52 -0300

    Adds tall rice standalone for testing

--- src/main/java/vectorwing/farmersdelight/blocks/RiceCropBlock.java
+++ src/main/java/vectorwing/farmersdelight/blocks/RiceCropBlock.java
@@ -5,32 +5,36 @@ import net.minecraft.fluid.Fluid;
 import net.minecraft.fluid.Fluids;
 import net.minecraft.fluid.IFluidState;
 import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.item.ItemStack;
 import net.minecraft.state.BooleanProperty;
+import net.minecraft.state.IntegerProperty;
 import net.minecraft.state.StateContainer;
 import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.tags.FluidTags;
 import net.minecraft.util.Direction;
-import net.minecraft.util.IItemProvider;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.IWorld;
 import net.minecraft.world.IWorldReader;
+import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
+import vectorwing.farmersdelight.init.ModBlocks;
 import vectorwing.farmersdelight.init.ModItems;
 
 import javax.annotation.Nullable;
+import java.util.Random;
 
-public class RiceCropBlock extends CropsBlock implements IWaterLoggable
+public class RiceCropBlock extends BushBlock implements IWaterLoggable, IGrowable
 {
+	public static final IntegerProperty AGE = IntegerProperty.create("age", 0, 4);
 	public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
 	private static final VoxelShape[] SHAPE_BY_AGE = new VoxelShape[] {
 			Block.makeCuboidShape(5.0D, 0.0D, 5.0D, 11.0D, 9.0D, 11.0D),
-			Block.makeCuboidShape(5.0D, 0.0D, 5.0D, 11.0D, 9.0D, 11.0D),
 			Block.makeCuboidShape(1.0D, 0.0D, 1.0D, 15.0D, 16.0D, 15.0D),
 			Block.makeCuboidShape(1.0D, 0.0D, 1.0D, 15.0D, 16.0D, 15.0D),
-			Block.makeCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 16.0D, 16.0D),
-			Block.makeCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 16.0D, 16.0D),
 			Block.makeCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 16.0D, 16.0D),
 			Block.makeCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 16.0D, 16.0D)};
 
@@ -39,9 +43,83 @@ public class RiceCropBlock extends CropsBlock implements IWaterLoggable
 		this.setDefaultState(this.getDefaultState().with(WATERLOGGED, true).with(AGE, 0));
 	}
 
-	@Override
-	protected IItemProvider getSeedsItem() {
-		return ModItems.RICE.get();
+	public void tick(BlockState state, ServerWorld worldIn, BlockPos pos, Random rand) {
+		super.tick(state, worldIn, pos, rand);
+		if (!worldIn.isAreaLoaded(pos, 1)) return; // Forge: prevent loading unloaded chunks when checking neighbor's light
+		if (worldIn.getLightSubtracted(pos, 0) >= 6) {
+			int i = this.getAge(state);
+			if (i < this.getMaxAge()) {
+				float f = getGrowthChance(this, worldIn, pos);
+				if (net.minecraftforge.common.ForgeHooks.onCropsGrowPre(worldIn, pos, state, rand.nextInt((int)(25.0F / f) + 1) == 0)) {
+					if (i == 5) {
+						TallRiceCropBlock tallRice = (TallRiceCropBlock) ModBlocks.TALL_RICE_CROP.get();
+						if (tallRice.getDefaultState().isValidPosition(worldIn, pos) && worldIn.isAirBlock(pos.up())) {
+							tallRice.placeAt(worldIn, pos, 2, 6);
+						}
+					}
+					worldIn.setBlockState(pos, this.withAge(i + 1), 2);
+					net.minecraftforge.common.ForgeHooks.onCropsGrowPost(worldIn, pos, state);
+				}
+			}
+		}
+
+	}
+
+	public void grow(World worldIn, BlockPos pos, BlockState state) {
+		int newAge = this.getAge(state) + this.getBonemealAgeIncrease(worldIn);
+		int maxAge = 7;
+		if (newAge > maxAge) newAge = maxAge;
+		if (newAge <= 5) {
+			worldIn.setBlockState(pos, state.with(AGE, newAge));
+		} else {
+			worldIn.setBlockState(pos.up(), Blocks.DIRT.getDefaultState(), 2);
+			// TODO: Place DoubleRiceCrop above this block
+		}
+	}
+
+	protected int getBonemealAgeIncrease(World worldIn) {
+		return MathHelper.nextInt(worldIn.rand, 2, 5);
+	}
+
+	protected static float getGrowthChance(Block blockIn, IBlockReader worldIn, BlockPos pos) {
+		float f = 1.0F;
+		BlockPos blockpos = pos.down();
+
+		for(int i = -1; i <= 1; ++i) {
+			for(int j = -1; j <= 1; ++j) {
+				float f1 = 0.0F;
+				BlockState blockstate = worldIn.getBlockState(blockpos.add(i, 0, j));
+				if (blockstate.canSustainPlant(worldIn, blockpos.add(i, 0, j), net.minecraft.util.Direction.UP, (net.minecraftforge.common.IPlantable)blockIn)) {
+					f1 = 1.0F;
+					if (blockstate.isFertile(worldIn, blockpos.add(i, 0, j))) {
+						f1 = 3.0F;
+					}
+				}
+
+				if (i != 0 || j != 0) {
+					f1 /= 4.0F;
+				}
+
+				f += f1;
+			}
+		}
+
+		BlockPos blockpos1 = pos.north();
+		BlockPos blockpos2 = pos.south();
+		BlockPos blockpos3 = pos.west();
+		BlockPos blockpos4 = pos.east();
+		boolean flag = blockIn == worldIn.getBlockState(blockpos3).getBlock() || blockIn == worldIn.getBlockState(blockpos4).getBlock();
+		boolean flag1 = blockIn == worldIn.getBlockState(blockpos1).getBlock() || blockIn == worldIn.getBlockState(blockpos2).getBlock();
+		if (flag && flag1) {
+			f /= 2.0F;
+		} else {
+			boolean flag2 = blockIn == worldIn.getBlockState(blockpos3.north()).getBlock() || blockIn == worldIn.getBlockState(blockpos4.north()).getBlock() || blockIn == worldIn.getBlockState(blockpos4.south()).getBlock() || blockIn == worldIn.getBlockState(blockpos3.south()).getBlock();
+			if (flag2) {
+				f /= 2.0F;
+			}
+		}
+
+		return f;
 	}
 
 	@Override
@@ -49,19 +127,41 @@ public class RiceCropBlock extends CropsBlock implements IWaterLoggable
 		return SHAPE_BY_AGE[state.get(this.getAgeProperty())];
 	}
 
-	@Override
-	protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
-		builder.add(AGE, WATERLOGGED);
+	public boolean isValidPosition(BlockState state, IWorldReader worldIn, BlockPos pos) {
+		IFluidState ifluidstate = worldIn.getFluidState(pos);
+		return super.isValidPosition(state, worldIn, pos) && ifluidstate.isTagged(FluidTags.WATER) && ifluidstate.getLevel() == 8;
 	}
 
 	@Override
 	protected boolean isValidGround(BlockState state, IBlockReader worldIn, BlockPos pos) {
-		return state.isSolidSide(worldIn, pos, Direction.UP) && state.getBlock() != Blocks.MAGMA_BLOCK;
+		return state.isSolidSide(worldIn, pos, Direction.UP) && state.getBlock() == Blocks.DIRT;
 	}
 
-	public boolean isValidPosition(BlockState state, IWorldReader worldIn, BlockPos pos) {
-		IFluidState ifluidstate = worldIn.getFluidState(pos);
-		return super.isValidPosition(state, worldIn, pos) && ifluidstate.isTagged(FluidTags.WATER) && ifluidstate.getLevel() == 8;
+	public IntegerProperty getAgeProperty() { return AGE; }
+
+	protected int getAge(BlockState state) {
+		return state.get(this.getAgeProperty());
+	}
+
+	public int getMaxAge() {
+		return 5;
+	}
+
+	public ItemStack getItem(IBlockReader worldIn, BlockPos pos, BlockState state) {
+		return new ItemStack(ModItems.RICE.get());
+	}
+
+	public BlockState withAge(int age) {
+		return this.getDefaultState().with(this.getAgeProperty(), Integer.valueOf(age));
+	}
+
+	public boolean isMaxAge(BlockState state) {
+		return state.get(this.getAgeProperty()) >= this.getMaxAge();
+	}
+
+	@Override
+	protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+		builder.add(AGE, WATERLOGGED);
 	}
 
 	public BlockState updatePostPlacement(BlockState stateIn, Direction facing, BlockState facingState, IWorld worldIn, BlockPos currentPos, BlockPos facingPos) {
@@ -77,6 +177,20 @@ public class RiceCropBlock extends CropsBlock implements IWaterLoggable
 	public BlockState getStateForPlacement(BlockItemUseContext context) {
 		IFluidState ifluidstate = context.getWorld().getFluidState(context.getPos());
 		return ifluidstate.isTagged(FluidTags.WATER) && ifluidstate.getLevel() == 8 ? super.getStateForPlacement(context) : null;
+	}
+
+	public boolean canGrow(IBlockReader worldIn, BlockPos pos, BlockState state, boolean isClient) {
+		return true;
+	}
+
+	public boolean canUseBonemeal(World worldIn, Random rand, BlockPos pos, BlockState state) {
+		return true;
+	}
+
+	@Override
+	public void grow(ServerWorld worldIn, Random rand, BlockPos pos, BlockState state)
+	{
+		this.grow(worldIn, pos, state);
 	}
 
 	@Override
