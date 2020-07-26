de774a37686534e4025775f89dab504db048b452
Lucas Barcellos <lucas.t.barcellos@gmail.com>
2020-07-26 20:22:36 -0300

    Add loot tables to wild patches

--- src/main/java/vectorwing/farmersdelight/blocks/WildPatchBlock.java
+++ src/main/java/vectorwing/farmersdelight/blocks/WildPatchBlock.java
@@ -5,10 +5,14 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.BushBlock;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
 
 public class WildPatchBlock extends BushBlock
 {
+	protected static final VoxelShape SHAPE = Block.makeCuboidShape(2.0D, 0.0D, 2.0D, 14.0D, 13.0D, 14.0D);
+
 	public WildPatchBlock(Properties properties)
 	{
 		super(properties);
@@ -16,6 +20,10 @@ public class WildPatchBlock extends BushBlock
 
 	protected boolean isValidGround(BlockState state, IBlockReader worldIn, BlockPos pos) {
 		return state.getBlock() == Blocks.DIRT || state.getBlock() == Blocks.GRASS_BLOCK || state.getBlock() == Blocks.SAND;
+	}
+
+	public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+		return SHAPE;
 	}
 
 	public Block.OffsetType getOffsetType() {
