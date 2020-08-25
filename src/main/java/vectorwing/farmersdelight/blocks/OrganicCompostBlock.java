25fecfa223cb1482ac7291559970c47ff35fecd1
grimmauld <soeren@benjos.de>
2020-08-25 23:06:14 +0200

    experimental staged composting

--- src/main/java/vectorwing/farmersdelight/blocks/OrganicCompostBlock.java
+++ src/main/java/vectorwing/farmersdelight/blocks/OrganicCompostBlock.java
@@ -1,25 +1,33 @@
 package vectorwing.farmersdelight.blocks;
 
+import mcp.MethodsReturnNonnullByDefault;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.MushroomBlock;
+import net.minecraft.state.IntegerProperty;
+import net.minecraft.state.StateContainer;
+import net.minecraft.tags.FluidTags;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.LightType;
 import net.minecraft.world.server.ServerWorld;
 import vectorwing.farmersdelight.registry.ModBlocks;
 
+import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.Random;
 
-public class OrganicCompostBlock extends Block
-{
-	public OrganicCompostBlock()
-	{
-		super(Properties.from(Blocks.DIRT));
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+public class OrganicCompostBlock extends Block {
+	public static IntegerProperty COMPOSTATION = IntegerProperty.create("compostation", 0, 7);
+
+	public OrganicCompostBlock() {
+		this(Properties.from(Blocks.DIRT));
 	}
 
-	public OrganicCompostBlock(Properties properties)
-	{
+	public OrganicCompostBlock(Properties properties) {
 		super(properties);
+		this.setDefaultState(super.getDefaultState().with(COMPOSTATION, 0));
 	}
 
 	@Override
@@ -28,24 +36,34 @@ public class OrganicCompostBlock extends Block
 	}
 
 	@Override
+	protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+		builder.add(COMPOSTATION);
+		super.fillStateContainer(builder);
+	}
+
+	@Override
+	@SuppressWarnings("deprecation")
 	public void randomTick(BlockState state, ServerWorld worldIn, BlockPos pos, Random random) {
 		if (worldIn.isRemote) return;
 
-		float chance = 0.05F;
-		if (state.getLightValue() <= 9) {
-			chance = chance + 0.1F;
+		float chance = 0F;
+
+		int maxLight = 0;
+		for (BlockPos blockpos : BlockPos.getAllInBoxMutable(pos.add(-1, 0, -1), pos.add(1, 0, 1))) {
+			BlockState adjacent = worldIn.getBlockState(blockpos);
+			if (adjacent.getBlock() instanceof MushroomBlock || adjacent.getBlock() instanceof OrganicCompostBlock || adjacent.getFluidState().isTagged(FluidTags.WATER))
+				chance += 0.04F;
+			int light = worldIn.getLightFor(LightType.BLOCK, blockpos);
+			if(light > maxLight)
+				maxLight = light;
 		}
-		boolean mushroomNearby = false;
-		for(BlockPos blockpos : BlockPos.getAllInBoxMutable(pos.add(-2, 1, -2), pos.add(2, 1, 2))) {
-			if (worldIn.getBlockState(blockpos).getBlock() instanceof MushroomBlock) {
-				mushroomNearby = true;
-			}
-		}
-		if (mushroomNearby)	{
-			chance = chance + 0.15F;
-		}
+		chance += 0.015F * (15 - maxLight);
+
 		if (worldIn.getRandom().nextFloat() <= chance) {
-			worldIn.setBlockState(pos, ModBlocks.MULCH.get().getDefaultState(), 2);
+			if (state.get(COMPOSTATION) == 7)
+				worldIn.setBlockState(pos, ModBlocks.MULCH.get().getDefaultState(), 2); // finished
+			else
+				worldIn.setBlockState(pos, state.with(COMPOSTATION, state.get(COMPOSTATION) + 1), 2); // next stage
 		}
 	}
 }
