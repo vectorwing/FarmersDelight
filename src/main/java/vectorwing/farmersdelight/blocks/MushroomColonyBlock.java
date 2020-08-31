a3e0b16b4c550e8a3e1aef7d00e13dc3b0e8930b
Lucas Barcellos <lucas.t.barcellos@gmail.com>
2020-08-30 22:10:58 -0300

    Tweak mushroom colony hitboxes

--- src/main/java/vectorwing/farmersdelight/blocks/MushroomColonyBlock.java
+++ src/main/java/vectorwing/farmersdelight/blocks/MushroomColonyBlock.java
@@ -9,8 +9,9 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.state.IntegerProperty;
 import net.minecraft.state.StateContainer;
 import net.minecraft.state.properties.BlockStateProperties;
-import net.minecraft.util.IItemProvider;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
@@ -21,6 +22,12 @@ import java.util.function.Supplier;
 
 public class MushroomColonyBlock extends BushBlock implements IGrowable
 {
+	protected static final VoxelShape[] SHAPE_BY_AGE = new VoxelShape[] {
+			Block.makeCuboidShape(4.0D, 0.0D, 4.0D, 12.0D, 8.0D, 12.0D),
+			Block.makeCuboidShape(3.0D, 0.0D, 3.0D, 13.0D, 10.0D, 13.0D),
+			Block.makeCuboidShape(2.0D, 0.0D, 2.0D, 14.0D, 12.0D, 14.0D),
+			Block.makeCuboidShape(1.0D, 0.0D, 1.0D, 15.0D, 14.0D, 15.0D),
+	};
 	public static final IntegerProperty COLONY_AGE = BlockStateProperties.AGE_0_3;
 	public final Supplier<Item> mushroomType;
 
@@ -29,6 +36,15 @@ public class MushroomColonyBlock extends BushBlock implements IGrowable
 		super(properties);
 		this.mushroomType = mushroomType;
 		this.setDefaultState(this.stateContainer.getBaseState().with(COLONY_AGE, 0));
+	}
+
+	@Override
+	public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+		return SHAPE_BY_AGE[state.get(this.getAgeProperty())];
+	}
+
+	public IntegerProperty getAgeProperty() {
+		return COLONY_AGE;
 	}
 
 	@Override
