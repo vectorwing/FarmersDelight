027124751409cb55f97be7c7aab1babad7748c35
Lucas Barcellos <lucas.t.barcellos@gmail.com>
2020-09-08 22:36:53 -0300

    Tweak Cooking Pot's flinging to clear blocks on its Y-level

--- src/main/java/vectorwing/farmersdelight/tile/CookingPotTileEntity.java
+++ src/main/java/vectorwing/farmersdelight/tile/CookingPotTileEntity.java
@@ -275,8 +275,11 @@ public class CookingPotTileEntity extends TileEntity implements INamedContainerP
 		for (int i = 0; i < MEAL_DISPLAY; ++i) {
 			if (itemHandler.getStackInSlot(i).hasContainerItem()) {
 				Direction direction = this.getBlockState().get(CookingPotBlock.FACING).rotateYCCW();
-				ItemEntity entity = new ItemEntity(world, pos.getX() + 0.5 + (direction.getXOffset() * 0.2), pos.getY() + 0.7, pos.getZ() + 0.5 + (direction.getZOffset() * 0.2), itemHandler.getStackInSlot(i).getContainerItem());
-				entity.setMotion(direction.getXOffset() * 0.1F, 0.2F, direction.getZOffset() * 0.1F);
+				double dropX = pos.getX() + 0.5 + (direction.getXOffset() * 0.25);
+				double dropY = pos.getY() + 0.7;
+				double dropZ = pos.getZ() + 0.5 + (direction.getZOffset() * 0.25);
+				ItemEntity entity = new ItemEntity(world, dropX, dropY, dropZ, itemHandler.getStackInSlot(i).getContainerItem());
+				entity.setMotion(direction.getXOffset() * 0.08F, 0.25F, direction.getZOffset() * 0.08F);
 				world.addEntity(entity);
 			}
 			if (!itemHandler.getStackInSlot(i).isEmpty())
