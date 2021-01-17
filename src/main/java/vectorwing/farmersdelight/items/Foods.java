65fcdc089b8295f78789968cdf0af40ace1b69db
Lucas Barcellos <lucas.t.barcellos@gmail.com>
2021-01-17 17:13:20 -0300

    Make popsicles always edible

--- src/main/java/vectorwing/farmersdelight/items/Foods.java
+++ src/main/java/vectorwing/farmersdelight/items/Foods.java
@@ -57,7 +57,7 @@ public class Foods
 
 	// Sweets
 	public static final Food POPSICLE = (new Food.Builder())
-			.hunger(3).saturation(0.2f).fastToEat()
+			.hunger(3).saturation(0.2f).fastToEat().setAlwaysEdible()
 			.effect(() -> new EffectInstance(Effects.FIRE_RESISTANCE, 160, 0), 1.0F).build();
 	public static final Food COOKIES = (new Food.Builder())
 			.hunger(2).saturation(0.1f).fastToEat().build();
