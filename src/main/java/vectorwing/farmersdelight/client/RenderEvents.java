65f9797beca67e4df2e4d76b7a10a9ba1c1e9793
Przemek <przemek@przemyk.xyz>
2021-01-18 08:17:01 +0100

    holding cooking pot now changes pose of both arms

---
+++ src/main/java/vectorwing/farmersdelight/client/RenderEvents.java
@@ -0,0 +1,28 @@
+package vectorwing.farmersdelight.client;
+
+import net.minecraft.client.entity.player.AbstractClientPlayerEntity;
+import net.minecraft.client.renderer.entity.model.BipedModel;
+import net.minecraft.client.renderer.entity.model.PlayerModel;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.util.Hand;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RenderPlayerEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import vectorwing.farmersdelight.registry.ModItems;
+
+@Mod.EventBusSubscriber(Dist.CLIENT)
+public class RenderEvents {
+
+    @SubscribeEvent
+    public static void playerRender(RenderPlayerEvent.Pre event) {
+        PlayerEntity playerEntity = event.getPlayer();
+        if (playerEntity.getHeldItem(Hand.MAIN_HAND).getItem() == ModItems.COOKING_POT.get() ||
+                playerEntity.getHeldItem(Hand.OFF_HAND).getItem() == ModItems.COOKING_POT.get()) {
+            PlayerModel<AbstractClientPlayerEntity> playerModel = event.getRenderer().getEntityModel();
+            playerModel.leftArmPose = BipedModel.ArmPose.ITEM;
+            playerModel.rightArmPose = BipedModel.ArmPose.ITEM;
+        }
+    }
+}
