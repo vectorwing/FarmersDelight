228a0c239f575f462f7b3a993ed9c357183effcb
Lucas Barcellos <lucas.t.barcellos@gmail.com>
2021-06-19 16:14:03 -0300

    Add experience to all Cooking Pot recipes

--- src/main/java/vectorwing/farmersdelight/data/recipes/CookingRecipes.java
+++ src/main/java/vectorwing/farmersdelight/data/recipes/CookingRecipes.java
@@ -14,8 +14,9 @@ import java.util.stream.Stream;
 
 public class CookingRecipes
 {
-	public static final int FAST_COOKING_TIME = 100;
-	public static final int SLOW_COOKING_TIME = 400;
+	public static final int FAST_COOKING = 100;
+	public static final int NORMAL_COOKING = 200;
+	public static final int SLOW_COOKING = 400;
 
 	public static void register(Consumer<IFinishedRecipe> consumer) {
 		cookMiscellaneous(consumer);
@@ -24,17 +25,17 @@ public class CookingRecipes
 	}
 
 	private static void cookMiscellaneous(Consumer<IFinishedRecipe> consumer) {
-		CookingPotRecipeBuilder.cookingRecipe(ModItems.HOT_COCOA.get(), 1)
+		CookingPotRecipeBuilder.cookingPotRecipe(ModItems.HOT_COCOA.get(), 1, NORMAL_COOKING, 0.35F)
 				.addIngredient(ForgeTags.MILK)
 				.addIngredient(Items.SUGAR)
 				.addIngredient(Items.COCOA_BEANS)
 				.addIngredient(Items.COCOA_BEANS)
 				.build(consumer);
-		CookingPotRecipeBuilder.cookingRecipe(ModItems.TOMATO_SAUCE.get(), 1, FAST_COOKING_TIME)
+		CookingPotRecipeBuilder.cookingPotRecipe(ModItems.TOMATO_SAUCE.get(), 1, FAST_COOKING, 0.1f)
 				.addIngredient(ForgeTags.CROPS_TOMATO)
 				.addIngredient(ForgeTags.CROPS_TOMATO)
 				.build(consumer);
-		CookingPotRecipeBuilder.cookingRecipe(ModItems.DOG_FOOD.get(), 1)
+		CookingPotRecipeBuilder.cookingPotRecipe(ModItems.DOG_FOOD.get(), 1, NORMAL_COOKING, 0.35F)
 				.addIngredient(Items.ROTTEN_FLESH)
 				.addIngredient(Items.BONE_MEAL)
 				.addIngredient(ModTags.WOLF_PREY)
@@ -43,16 +44,16 @@ public class CookingRecipes
 	}
 
 	private static void cookMinecraftSoups(Consumer<IFinishedRecipe> consumer) {
-		CookingPotRecipeBuilder.cookingRecipe(Items.MUSHROOM_STEW, 1, Items.BOWL)
+		CookingPotRecipeBuilder.cookingPotRecipe(Items.MUSHROOM_STEW, 1, NORMAL_COOKING, 0.2F, Items.BOWL)
 				.addIngredient(Items.BROWN_MUSHROOM)
 				.addIngredient(Items.RED_MUSHROOM)
 				.build(consumer);
-		CookingPotRecipeBuilder.cookingRecipe(Items.BEETROOT_SOUP, 1, Items.BOWL)
+		CookingPotRecipeBuilder.cookingPotRecipe(Items.BEETROOT_SOUP, 1, NORMAL_COOKING, 0.2F, Items.BOWL)
 				.addIngredient(Items.BEETROOT)
 				.addIngredient(Items.BEETROOT)
 				.addIngredient(Items.BEETROOT)
 				.build(consumer);
-		CookingPotRecipeBuilder.cookingRecipe(Items.RABBIT_STEW, 1, Items.BOWL)
+		CookingPotRecipeBuilder.cookingPotRecipe(Items.RABBIT_STEW, 1, NORMAL_COOKING, 0.35F, Items.BOWL)
 				.addIngredient(Items.BAKED_POTATO)
 				.addIngredient(Ingredient.fromItems(Items.RABBIT, Items.COOKED_RABBIT))
 				.addIngredient(Items.CARROT)
@@ -61,31 +62,31 @@ public class CookingRecipes
 	}
 
 	private static void cookMeals(Consumer<IFinishedRecipe> consumer) {
-		CookingPotRecipeBuilder.cookingRecipe(ModItems.BAKED_COD_STEW.get(), 1)
+		CookingPotRecipeBuilder.cookingPotRecipe(ModItems.BAKED_COD_STEW.get(), 1, NORMAL_COOKING, 0.35F)
 				.addIngredient(ForgeTags.RAW_FISHES_COD)
 				.addIngredient(Items.POTATO)
 				.addIngredient(ForgeTags.EGGS)
 				.addIngredient(ForgeTags.CROPS_TOMATO)
 				.build(consumer);
-		CookingPotRecipeBuilder.cookingRecipe(ModItems.BEEF_STEW.get(), 1)
+		CookingPotRecipeBuilder.cookingPotRecipe(ModItems.BEEF_STEW.get(), 1, NORMAL_COOKING, 0.35F)
 				.addIngredient(ForgeTags.RAW_BEEF)
 				.addIngredient(Items.CARROT)
 				.addIngredient(Items.POTATO)
 				.build(consumer);
-		CookingPotRecipeBuilder.cookingRecipe(ModItems.CABBAGE_ROLLS.get(), 1, FAST_COOKING_TIME)
+		CookingPotRecipeBuilder.cookingPotRecipe(ModItems.CABBAGE_ROLLS.get(), 1, FAST_COOKING, 0.1F)
 				.addIngredient(ForgeTags.CROPS_CABBAGE)
 				.addIngredient(ModTags.CABBAGE_ROLL_INGREDIENTS)
 				.build(consumer);
-		CookingPotRecipeBuilder.cookingRecipe(ModItems.CHICKEN_SOUP.get(), 1)
+		CookingPotRecipeBuilder.cookingPotRecipe(ModItems.CHICKEN_SOUP.get(), 1, NORMAL_COOKING, 0.35F)
 				.addIngredient(ForgeTags.RAW_CHICKEN)
 				.addIngredient(Items.CARROT)
 				.addIngredient(ForgeTags.SALAD_INGREDIENTS)
 				.addIngredient(ForgeTags.VEGETABLES)
 				.build(consumer);
-		CookingPotRecipeBuilder.cookingRecipe(ModItems.COOKED_RICE.get(), 1, FAST_COOKING_TIME)
+		CookingPotRecipeBuilder.cookingPotRecipe(ModItems.COOKED_RICE.get(), 1, FAST_COOKING, 0.1F)
 				.addIngredient(ForgeTags.CROPS_RICE)
 				.build(consumer);
-		CookingPotRecipeBuilder.cookingRecipe(ModItems.DUMPLINGS.get(), 2)
+		CookingPotRecipeBuilder.cookingPotRecipe(ModItems.DUMPLINGS.get(), 2, NORMAL_COOKING, 0.35F)
 				.addIngredient(ForgeTags.PASTA)
 				.addIngredient(ForgeTags.CROPS_CABBAGE)
 				.addIngredient(ForgeTags.CROPS_ONION)
@@ -96,58 +97,52 @@ public class CookingRecipes
 						new Ingredient.SingleItemList(new ItemStack(Items.BROWN_MUSHROOM))
 				)))
 				.build(consumer);
-		CookingPotRecipeBuilder.cookingRecipe(ModItems.FISH_STEW.get(), 1)
+		CookingPotRecipeBuilder.cookingPotRecipe(ModItems.FISH_STEW.get(), 1, NORMAL_COOKING, 0.35F)
 				.addIngredient(ForgeTags.RAW_FISHES)
 				.addIngredient(ModItems.TOMATO_SAUCE.get())
 				.addIngredient(ForgeTags.CROPS_ONION)
 				.build(consumer);
-		CookingPotRecipeBuilder.cookingRecipe(ModItems.FRIED_RICE.get(), 1)
+		CookingPotRecipeBuilder.cookingPotRecipe(ModItems.FRIED_RICE.get(), 1, NORMAL_COOKING, 0.35F)
 				.addIngredient(ForgeTags.CROPS_RICE)
 				.addIngredient(ForgeTags.EGGS)
 				.addIngredient(Items.CARROT)
 				.addIngredient(ForgeTags.CROPS_ONION)
 				.build(consumer);
-		CookingPotRecipeBuilder.cookingRecipe(ModItems.NOODLE_SOUP.get(), 1)
+		CookingPotRecipeBuilder.cookingPotRecipe(ModItems.NOODLE_SOUP.get(), 1, NORMAL_COOKING, 0.35F)
 				.addIngredient(ForgeTags.PASTA)
 				.addIngredient(ForgeTags.COOKED_EGGS)
 				.addIngredient(Items.DRIED_KELP)
 				.addIngredient(ForgeTags.RAW_PORK)
 				.build(consumer);
-		CookingPotRecipeBuilder.cookingRecipe(ModItems.PASTA_WITH_MEATBALLS.get(), 1)
+		CookingPotRecipeBuilder.cookingPotRecipe(ModItems.PASTA_WITH_MEATBALLS.get(), 1, NORMAL_COOKING, 0.35F)
 				.addIngredient(ModItems.MINCED_BEEF.get())
 				.addIngredient(ForgeTags.PASTA)
 				.addIngredient(ModItems.TOMATO_SAUCE.get())
 				.build(consumer);
-		CookingPotRecipeBuilder.cookingRecipe(ModItems.PASTA_WITH_MUTTON_CHOP.get(), 1)
+		CookingPotRecipeBuilder.cookingPotRecipe(ModItems.PASTA_WITH_MUTTON_CHOP.get(), 1, NORMAL_COOKING, 0.35F)
 				.addIngredient(ForgeTags.RAW_MUTTON)
 				.addIngredient(ForgeTags.PASTA)
 				.addIngredient(ModItems.TOMATO_SAUCE.get())
 				.build(consumer);
-		CookingPotRecipeBuilder.cookingRecipe(ModItems.PUMPKIN_SOUP.get(), 1)
+		CookingPotRecipeBuilder.cookingPotRecipe(ModItems.PUMPKIN_SOUP.get(), 1, NORMAL_COOKING, 0.35F)
 				.addIngredient(ModItems.PUMPKIN_SLICE.get())
 				.addIngredient(ForgeTags.SALAD_INGREDIENTS)
 				.addIngredient(ForgeTags.RAW_PORK)
 				.addIngredient(ForgeTags.MILK)
 				.build(consumer);
-		CookingPotRecipeBuilder.cookingRecipe(ModItems.RATATOUILLE.get(), 1)
+		CookingPotRecipeBuilder.cookingPotRecipe(ModItems.RATATOUILLE.get(), 1, NORMAL_COOKING, 0.35F)
 				.addIngredient(ForgeTags.CROPS_TOMATO)
 				.addIngredient(ForgeTags.CROPS_ONION)
 				.addIngredient(Items.BEETROOT)
 				.addIngredient(ForgeTags.VEGETABLES)
 				.build(consumer);
-//		CookingPotRecipeBuilder.cookingRecipe(ModItems.SHEPHERDS_PIE.get(), 1)
-//				.addIngredient(Items.MUTTON)
-//				.addIngredient(Items.BAKED_POTATO)
-//				.addIngredient(ForgeTags.CROPS_ONION)
-//				.addIngredient(ForgeTags.MILK)
-//				.build(consumer);
-		CookingPotRecipeBuilder.cookingRecipe(ModItems.SQUID_INK_PASTA.get(), 1)
+		CookingPotRecipeBuilder.cookingPotRecipe(ModItems.SQUID_INK_PASTA.get(), 1, NORMAL_COOKING, 0.35F)
 				.addIngredient(ForgeTags.RAW_FISHES)
 				.addIngredient(ForgeTags.PASTA)
 				.addIngredient(ForgeTags.CROPS_TOMATO)
 				.addIngredient(Items.INK_SAC)
 				.build(consumer);
-		CookingPotRecipeBuilder.cookingRecipe(ModItems.STUFFED_PUMPKIN_BLOCK.get(), 1, Items.PUMPKIN, SLOW_COOKING_TIME)
+		CookingPotRecipeBuilder.cookingPotRecipe(ModItems.STUFFED_PUMPKIN_BLOCK.get(), 1, SLOW_COOKING, 0.5F, Items.PUMPKIN)
 				.addIngredient(ForgeTags.CROPS_RICE)
 				.addIngredient(ForgeTags.VEGETABLES)
 				.addIngredient(ModItems.TOMATO_SAUCE.get())
@@ -155,14 +150,14 @@ public class CookingRecipes
 				.addIngredient(ModItems.BROWN_MUSHROOM_COLONY.get())
 				.addIngredient(Items.SWEET_BERRIES)
 				.build(consumer);
-		CookingPotRecipeBuilder.cookingRecipe(ModItems.VEGETABLE_NOODLES.get(), 1)
+		CookingPotRecipeBuilder.cookingPotRecipe(ModItems.VEGETABLE_NOODLES.get(), 1, NORMAL_COOKING, 0.35F)
 				.addIngredient(Items.CARROT)
 				.addIngredient(Items.BROWN_MUSHROOM)
 				.addIngredient(ForgeTags.PASTA)
 				.addIngredient(ForgeTags.SALAD_INGREDIENTS)
 				.addIngredient(ForgeTags.VEGETABLES)
 				.build(consumer);
-		CookingPotRecipeBuilder.cookingRecipe(ModItems.VEGETABLE_SOUP.get(), 1)
+		CookingPotRecipeBuilder.cookingPotRecipe(ModItems.VEGETABLE_SOUP.get(), 1, NORMAL_COOKING, 0.35F)
 				.addIngredient(Items.CARROT)
 				.addIngredient(Items.POTATO)
 				.addIngredient(Items.BEETROOT)
