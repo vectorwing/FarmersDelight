7a7e4eab4d5cebaf17e2b71dd23f64d84ca43199
Lucas Barcellos <lucas.t.barcellos@gmail.com>
2021-07-07 02:15:31 -0300

    Render Skillet contents as a misaligned stack

--- src/main/java/vectorwing/farmersdelight/client/tileentity/renderer/SkilletTileEntityRenderer.java
+++ src/main/java/vectorwing/farmersdelight/client/tileentity/renderer/SkilletTileEntityRenderer.java
@@ -3,10 +3,10 @@ package vectorwing.farmersdelight.client.tileentity.renderer;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
-import net.minecraft.client.renderer.WorldRenderer;
 import net.minecraft.client.renderer.model.ItemCameraTransforms;
 import net.minecraft.client.renderer.tileentity.TileEntityRenderer;
 import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.vector.Vector3f;
@@ -14,8 +14,12 @@ import net.minecraftforge.items.IItemHandler;
 import vectorwing.farmersdelight.blocks.StoveBlock;
 import vectorwing.farmersdelight.tile.SkilletTileEntity;
 
+import java.util.Random;
+
 public class SkilletTileEntityRenderer extends TileEntityRenderer<SkilletTileEntity>
 {
+	private final Random random = new Random();
+
 	public SkilletTileEntityRenderer(TileEntityRendererDispatcher rendererDispatcherIn) {
 		super(rendererDispatcherIn);
 	}
@@ -26,26 +30,46 @@ public class SkilletTileEntityRenderer extends TileEntityRenderer<SkilletTileEnt
 		IItemHandler inventory = tileEntityIn.getInventory();
 
 		ItemStack stack = inventory.getStackInSlot(0);
+		int seed = stack.isEmpty() ? 187 : Item.getIdFromItem(stack.getItem()) + stack.getDamage();
+		this.random.setSeed(seed);
 
 		if (!stack.isEmpty()) {
-			matrixStackIn.push();
+			int itemRenderCount = this.getModelCount(stack);
+			for (int i = 0; i < itemRenderCount; i++) {
+				matrixStackIn.push();
 
-			// Center item on top of the skillet
-			matrixStackIn.translate(0.5D, 0.1D, 0.5D);
+				// Stack up items in the skillet, with a slight offset per item
+				float xOffset = (this.random.nextFloat() * 2.0F - 1.0F) * 0.15F * 0.5F;
+				float zOffset = (this.random.nextFloat() * 2.0F - 1.0F) * 0.15F * 0.5F;
+				matrixStackIn.translate(0.5D + xOffset, 0.1D + 0.03 * (i + 1), 0.5D + zOffset);
 
-			// Rotate item to face the stove's front side
-			float degrees = -direction.getHorizontalAngle();
-			matrixStackIn.rotate(Vector3f.YP.rotationDegrees(degrees));
+				// Rotate item to face the skillet's front side
+				float degrees = -direction.getHorizontalAngle();
+				matrixStackIn.rotate(Vector3f.YP.rotationDegrees(degrees));
 
-			// Rotate item flat on the stove. Use X and Y from now on
-			matrixStackIn.rotate(Vector3f.XP.rotationDegrees(90.0F));
+				// Rotate item flat on the skillet. Use X and Y from now on
+				matrixStackIn.rotate(Vector3f.XP.rotationDegrees(90.0F));
 
-			// Resize the items
-			matrixStackIn.scale(0.6F, 0.6F, 0.6F);
+				// Resize the items
+				matrixStackIn.scale(0.4F, 0.4F, 0.4F);
 
-			if (tileEntityIn.getWorld() != null)
-				Minecraft.getInstance().getItemRenderer().renderItem(stack, ItemCameraTransforms.TransformType.FIXED, combinedLightIn, combinedOverlayIn, matrixStackIn, bufferIn);
-			matrixStackIn.pop();
+				if (tileEntityIn.getWorld() != null)
+					Minecraft.getInstance().getItemRenderer().renderItem(stack, ItemCameraTransforms.TransformType.FIXED, combinedLightIn, combinedOverlayIn, matrixStackIn, bufferIn);
+				matrixStackIn.pop();
+			}
 		}
+	}
+
+	protected int getModelCount(ItemStack stack) {
+		if (stack.getCount() > 48) {
+			return 5;
+		} else if (stack.getCount() > 32) {
+			return 4;
+		} else if (stack.getCount() > 16) {
+			return 3;
+		} else if (stack.getCount() > 1) {
+			return 2;
+		}
+		return 1;
 	}
 }
