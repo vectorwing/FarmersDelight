5345da9ac62164d3b506c1af8504767b6f31a028
Lucas Barcellos <lucas.t.barcellos@gmail.com>
2021-10-13 20:57:38 -0300

    Add mixin to hide break progress on canvas rugs

---
+++ src/main/java/vectorwing/farmersdelight/mixin/HideBlockBreakProgressMixin.java
@@ -0,0 +1,24 @@
+package vectorwing.farmersdelight.mixin;
+
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.vertex.IVertexBuilder;
+import net.minecraft.block.BlockState;
+import net.minecraft.client.renderer.BlockRendererDispatcher;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockDisplayReader;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import vectorwing.farmersdelight.registry.ModBlocks;
+
+@Mixin(BlockRendererDispatcher.class)
+public abstract class HideBlockBreakProgressMixin
+{
+	@Inject(method = "renderBlockDamage", at = @At("HEAD"), cancellable = true)
+	private void hideBlockDamage(BlockState blockStateIn, BlockPos posIn, IBlockDisplayReader lightReaderIn, MatrixStack matrixStackIn, IVertexBuilder vertexBuilderIn, CallbackInfo ci) {
+		if (blockStateIn.getBlock() == ModBlocks.CANVAS_RUG.get()) {
+			ci.cancel();
+		}
+	}
+}
