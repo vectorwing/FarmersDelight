14e164ee0ef90daa7525ac96acf8d288ded2a64b
Lucas Barcellos <lucas.t.barcellos@gmail.com>
2021-10-19 01:16:20 -0300

    Update Nourishment effect
    - Remove obsolete access transformer and use getFoodExhaustion;
    - Update exhaustion subtraction

--- src/main/java/vectorwing/farmersdelight/effects/NourishmentEffect.java
+++ src/main/java/vectorwing/farmersdelight/effects/NourishmentEffect.java
@@ -10,30 +10,27 @@ import net.minecraft.world.level.GameRules;
 public class NourishmentEffect extends MobEffect
 {
 	/**
-	 * This effect makes the player immune to accumulating food exhaustion. It is useless to all other entities.
-	 * Normal exhausting actions such as running, jumping and attacking will not drain hunger or saturation.
+	 * This effect prevents hunger loss by constantly decreasing the exhaustion level.
 	 * If the player can spend saturation to heal damage, the effect halts to let them do so, until they can't any more.
 	 * This means players can grow hungry by healing damage, but no further than 1.5 points, allowing them to eat more and keep healing.
-	 * <p>
-	 * This method employs an Access Transformer exclusively to read the exhaustion value; it calls the proper functions to change it.
 	 */
 	public NourishmentEffect() {
 		super(MobEffectCategory.BENEFICIAL, 0);
 	}
 
 	public void applyEffectTick(LivingEntity entityLivingBaseIn, int amplifier) {
-		if (!entityLivingBaseIn.getCommandSenderWorld().isClientSide && entityLivingBaseIn instanceof Player) {
-			Player player = ((Player) entityLivingBaseIn);
-			FoodData foodStats = player.getFoodData();
+		if (!entityLivingBaseIn.getCommandSenderWorld().isClientSide && entityLivingBaseIn instanceof Player player) {
+			FoodData foodData = player.getFoodData();
 			boolean isPlayerHealingWithSaturation =
 					player.level.getGameRules().getBoolean(GameRules.RULE_NATURAL_REGENERATION)
 							&& player.isHurt()
-							&& foodStats.getSaturationLevel() > 0.0F
-							&& foodStats.getFoodLevel() >= 20;
+							&& foodData.getSaturationLevel() > 0.0F
+							&& foodData.getFoodLevel() >= 20;
 			if (!isPlayerHealingWithSaturation) {
-				float exhaustion = player.getFoodData().exhaustionLevel;
-				if (exhaustion > 0.1F) {
-					player.causeFoodExhaustion(-0.1F);
+				float exhaustion = foodData.getExhaustionLevel();
+				float reduction = Math.min(exhaustion, 0.1F);
+				if (exhaustion > 0.0F) {
+					player.causeFoodExhaustion(-reduction);
 				}
 			}
 		}
