3736566e21ddabe54df4f09191b5ea7def1f64ee
Lucas Barcellos <lucas.t.barcellos@gmail.com>
2022-01-01 18:30:52 -0300

    Add limits to Comfort, update langs

--- src/main/java/vectorwing/farmersdelight/common/effect/ComfortEffect.java
+++ src/main/java/vectorwing/farmersdelight/common/effect/ComfortEffect.java
@@ -1,59 +1,34 @@
 package vectorwing.farmersdelight.common.effect;
 
-import com.google.common.collect.Sets;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.effect.MobEffect;
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.effect.MobEffectCategory;
-import net.minecraft.world.effect.MobEffects;
-import net.minecraftforge.event.entity.living.PotionEvent;
-import net.minecraftforge.eventbus.api.Event;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
-import vectorwing.farmersdelight.FarmersDelight;
-import vectorwing.farmersdelight.common.registry.ModEffects;
-
-import java.util.Set;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.potion.Effect;
+import net.minecraft.potion.EffectType;
+import net.minecraft.potion.Effects;
 
 @SuppressWarnings("unused")
 public class ComfortEffect extends MobEffect
 {
-	public static final Set<MobEffect> COMFORT_IMMUNITIES = Sets.newHashSet(MobEffects.MOVEMENT_SLOWDOWN, MobEffects.WEAKNESS, MobEffects.HUNGER);
-
 	/**
-	 * This effect makes the entity immune to negative effects related to cold and sickness.
-	 * The entity cannot freeze inside powder snow, and cannot receive Slowness or Weakness.
+	 * This effect extends the player's natural regeneration, regardless of how hungry they are.
+	 * Comfort does not care for amplifiers; it will always heal at the same slow pace.
+	 * If the player has saturation to spend, or has the Regeneration effect, Comfort does nothing.
 	 */
 	public ComfortEffect() {
 		super(MobEffectCategory.BENEFICIAL, 0);
 	}
 
-	@Mod.EventBusSubscriber(modid = FarmersDelight.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
-	public static class ComfortEvent
-	{
-		@SubscribeEvent
-		public static void onBadEffectApplicable(PotionEvent.PotionApplicableEvent event) {
-			MobEffectInstance effect = event.getPotionEffect();
-			LivingEntity entity = event.getEntityLiving();
-			if (entity.getEffect(ModEffects.COMFORT.get()) != null && COMFORT_IMMUNITIES.contains(effect.getEffect())) {
-				event.setResult(Event.Result.DENY);
-			}
-		}
-
-		@SubscribeEvent
-		public static void onBadEffectApplied(PotionEvent.PotionAddedEvent event) {
-			MobEffectInstance addedEffect = event.getPotionEffect();
-			LivingEntity entity = event.getEntityLiving();
-			if (addedEffect.getEffect().equals(ModEffects.COMFORT.get())) {
-				for (MobEffect effect : COMFORT_IMMUNITIES) {
-					entity.removeEffect(effect);
-				}
-			}
-		}
-	}
-
 	@Override
 	public void applyEffectTick(LivingEntity entity, int amplifier) {
+		if (entity.hasEffect(Effects.REGENERATION)) {
+			return;
+		}
+		if (entity instanceof PlayerEntity) {
+			PlayerEntity player = (PlayerEntity) entity;
+			if (player.getFoodData().getSaturationLevel() > 0.0) {
+				return;
+			}
+		}
 		if (entity.getHealth() < entity.getMaxHealth()) {
 			entity.heal(1.0F);
 		}
