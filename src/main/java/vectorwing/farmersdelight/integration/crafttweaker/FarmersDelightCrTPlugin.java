0089b053e62543e4279130a0688ad0ecab9563d9
Witixin1512 <73248264+Witixin1512@users.noreply.github.com>
2022-11-16 20:55:34 +0100

    Add CrT plugin to support replacement

---
+++ src/main/java/vectorwing/farmersdelight/integration/crafttweaker/FarmersDelightCrTPlugin.java
@@ -0,0 +1,19 @@
+package vectorwing.farmersdelight.integration.crafttweaker;
+
+import com.blamejared.crafttweaker.api.plugin.CraftTweakerPlugin;
+import com.blamejared.crafttweaker.api.plugin.ICraftTweakerPlugin;
+import com.blamejared.crafttweaker.api.plugin.IRecipeComponentRegistrationHandler;
+import vectorwing.farmersdelight.FarmersDelight;
+import vectorwing.farmersdelight.integration.crafttweaker.handlers.RecipeHandlerUtils;
+
+@CraftTweakerPlugin(FarmersDelight.MODID + ":crafttweaker_plugin")
+public class FarmersDelightCrTPlugin implements ICraftTweakerPlugin {
+
+    @Override
+    public void registerRecipeComponents(IRecipeComponentRegistrationHandler handler) {
+
+        handler.registerRecipeComponent(RecipeHandlerUtils.SOUND_COMPONENT);
+        handler.registerRecipeComponent(RecipeHandlerUtils.CONTAINER_COMPONENT);
+        handler.registerRecipeComponent(RecipeHandlerUtils.COOKING_TAB_COMPONENT);
+    }
+}
