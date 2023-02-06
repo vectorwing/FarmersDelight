40e98162dda1200cdfda527a04f8b82cf2716a92
Edgar Onghena <dev@edgar.bzh>
2023-02-06 22:44:44 +0100

    Fix hand-held skillet ingredient rendering
    fixes #515

--- src/main/java/vectorwing/farmersdelight/client/model/WrappedItemModel.java
+++ src/main/java/vectorwing/farmersdelight/client/model/WrappedItemModel.java
@@ -3,7 +3,10 @@ package vectorwing.farmersdelight.client.model;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.renderer.block.model.ItemTransforms;
 import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.client.model.BakedModelWrapper;
+
+import java.util.List;
 
 public class WrappedItemModel<T extends BakedModel> extends BakedModelWrapper<T>
 {
@@ -20,5 +23,10 @@ public class WrappedItemModel<T extends BakedModel> extends BakedModelWrapper<T>
 	public BakedModel applyTransform(ItemTransforms.TransformType cameraTransformType, PoseStack poseStack, boolean applyLeftHandTransform) {
 		BakedModel model = super.applyTransform(cameraTransformType, poseStack, applyLeftHandTransform);
 		return model.equals(this) || model instanceof WrappedItemModel ? this : new WrappedItemModel<>(model);
+	}
+
+	@Override
+	public List<BakedModel> getRenderPasses(ItemStack itemStack, boolean fabulous) {
+		return List.of(this);
 	}
 }
