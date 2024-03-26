ba812de0819c4fa27e41a018d37011e7f84c2f39
MehVahdJukaar <matteogozzi1998@gmail.com>
2024-03-26 23:07:20 +0100

    Very awesome skillet stuff. Flipping included

--- src/main/java/vectorwing/farmersdelight/FarmersDelight.java
+++ src/main/java/vectorwing/farmersdelight/FarmersDelight.java
@@ -17,6 +17,13 @@ import org.apache.logging.log4j.Logger;
 import vectorwing.farmersdelight.client.ClientSetup;
 import vectorwing.farmersdelight.common.CommonSetup;
 import vectorwing.farmersdelight.common.Configuration;
+import vectorwing.farmersdelight.common.block.entity.CookingPotBlockEntity;
+import vectorwing.farmersdelight.common.block.entity.CuttingBoardBlockEntity;
+import vectorwing.farmersdelight.common.event.CommonEvents;
+import vectorwing.farmersdelight.common.event.VillagerEvents;
+import vectorwing.farmersdelight.common.item.DogFoodItem;
+import vectorwing.farmersdelight.common.item.HorseFeedItem;
+import vectorwing.farmersdelight.common.item.KnifeItem;
 import vectorwing.farmersdelight.common.registry.*;
 import vectorwing.farmersdelight.common.world.VillageStructures;
 
@@ -71,5 +78,6 @@ public class FarmersDelight
 		MinecraftForge.EVENT_BUS.addListener(VillageStructures::addNewVillageBuilding);
 
 		MinecraftForge.EVENT_BUS.register(this);
+		ModNetworking.init();
 	}
 }
