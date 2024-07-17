4f7655d32ff0ad5ea3403d0e82e3482048150a07
Lucas Barcellos <lucas.t.barcellos@gmail.com>
2024-07-17 20:56:12 -0300

    Retire the scalable icon renderer method

--- src/main/java/vectorwing/farmersdelight/common/utility/ClientRenderUtils.java
+++ src/main/java/vectorwing/farmersdelight/common/utility/ClientRenderUtils.java
@@ -16,47 +16,11 @@ import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
 
 /**
- * Util for helping with rendering elements across the mod, when vanilla methods don't expose enough to use.
+ * Util for helping with rendering elements across the mod.
  */
 public class ClientRenderUtils
 {
 	public static boolean isCursorInsideBounds(int iconX, int iconY, int iconWidth, int iconHeight, double cursorX, double cursorY) {
 		return iconX <= cursorX && cursorX < iconX + iconWidth && iconY <= cursorY && cursorY < iconY + iconHeight;
-	}
-
-	/**
-	 * Renders an Item into the GUI, allowing the size to be defined instead of hardcoded.
-	 * This function is ripped right from the game's rendering code. I am probably doing something stupid.
-	 */
-	public static void renderItemIntoGUIScalable(ItemStack stack, float width, float height, BakedModel bakedmodel, ItemRenderer renderer, TextureManager textureManager) {
-		textureManager.getTexture(TextureAtlas.LOCATION_BLOCKS).setFilter(false, false);
-		RenderSystem.setShaderTexture(0, TextureAtlas.LOCATION_BLOCKS);
-		RenderSystem.enableBlend();
-		RenderSystem.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
-
-		PoseStack modelViewStack = RenderSystem.getModelViewStack();
-		modelViewStack.pushPose();
-//		modelViewStack.translate((float) 0, (float) 0, 100.0F + renderer.blitOffset);
-		modelViewStack.translate(8.0F, 8.0F, 0.0F);
-		modelViewStack.scale(1.0F, -1.0F, 1.0F);
-		modelViewStack.scale(width, height, 48.0F);
-		RenderSystem.applyModelViewMatrix();
-
-		PoseStack poseStack = new PoseStack();
-		MultiBufferSource.BufferSource multibuffersource$buffersource = Minecraft.getInstance().renderBuffers().bufferSource();
-		boolean usesBlockLight = !bakedmodel.usesBlockLight();
-		if (usesBlockLight) {
-			Lighting.setupForFlatItems();
-		}
-
-		renderer.render(stack, ItemDisplayContext.GUI, false, poseStack, multibuffersource$buffersource, 15728880, OverlayTexture.NO_OVERLAY, bakedmodel);
-		multibuffersource$buffersource.endBatch();
-		RenderSystem.enableDepthTest();
-		if (usesBlockLight) {
-			Lighting.setupFor3DItems();
-		}
-
-		poseStack.popPose();
-		RenderSystem.applyModelViewMatrix();
 	}
 }
