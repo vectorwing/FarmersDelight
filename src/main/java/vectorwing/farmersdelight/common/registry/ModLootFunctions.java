a11d4b953c5216132ac96f683b11f8b50e8de3de
Lucas Barcellos <lucas.t.barcellos@gmail.com>
2024-07-19 10:56:45 -0300

    Retire CopyMealFunction

--- src/main/java/vectorwing/farmersdelight/common/registry/ModLootFunctions.java
+++ src/main/java/vectorwing/farmersdelight/common/registry/ModLootFunctions.java
@@ -4,7 +4,6 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.world.level.storage.loot.functions.LootItemFunctionType;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import vectorwing.farmersdelight.FarmersDelight;
-import vectorwing.farmersdelight.common.loot.function.CopyMealFunction;
 import vectorwing.farmersdelight.common.loot.function.CopySkilletFunction;
 import vectorwing.farmersdelight.common.loot.function.SmokerCookFunction;
 
@@ -14,7 +13,6 @@ public class ModLootFunctions
 {
 	public static final DeferredRegister<LootItemFunctionType<?>> LOOT_FUNCTIONS = DeferredRegister.create(Registries.LOOT_FUNCTION_TYPE, FarmersDelight.MODID);
 
-	public static final Supplier<LootItemFunctionType<CopyMealFunction>> COPY_MEAL = LOOT_FUNCTIONS.register("copy_meal", () -> new LootItemFunctionType<>(CopyMealFunction.CODEC));
 	public static final Supplier<LootItemFunctionType<CopySkilletFunction>> COPY_SKILLET = LOOT_FUNCTIONS.register("copy_skillet", () -> new LootItemFunctionType<>(CopySkilletFunction.CODEC));
 	public static final Supplier<LootItemFunctionType<SmokerCookFunction>> SMOKER_COOK = LOOT_FUNCTIONS.register("smoker_cook", () -> new LootItemFunctionType<>(SmokerCookFunction.CODEC));
 }
