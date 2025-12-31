8b9d1231e5b0e586072c958a5c0118a5b0a90557
TNWing <trevngwing@gmail.com>
2025-12-31 13:22:15 -0500

    another check for comfort effect
    IF the player has saturation but not enough to trigger natural regeneration or saturation healing, comfort can still heal

--- src/main/java/vectorwing/farmersdelight/common/effect/ComfortEffect.java
+++ src/main/java/vectorwing/farmersdelight/common/effect/ComfortEffect.java
@@ -33,8 +33,8 @@ public class ComfortEffect extends MobEffect
 			if (player.getFoodData().getFoodLevel()>=18){
 				return;
 			}
-			if (player.getFoodData().getSaturationLevel() > 0.0) {
-				return;
+			if (player.getFoodData().getSaturationLevel() > 0.0 && player.getFoodData().getFoodLevel()<18) {
+					return;
 			}
 
 		}
