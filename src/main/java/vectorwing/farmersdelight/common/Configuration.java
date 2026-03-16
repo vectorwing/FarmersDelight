f4c9cbb6d78ff426247679ae295a4f2dca12d44c
Lucas Barcellos <lucas.t.barcellos@gmail.com>
2026-03-16 13:21:22 -0300

    Remove unused world gen configs

--- src/main/java/vectorwing/farmersdelight/common/Configuration.java
+++ src/main/java/vectorwing/farmersdelight/common/Configuration.java
@@ -44,24 +44,6 @@ public class Configuration
 	public static ForgeConfigSpec.BooleanValue GENERATE_FD_CHEST_LOOT;
 	public static ForgeConfigSpec.BooleanValue GENERATE_VILLAGE_COMPOST_HEAPS;
 	public static ForgeConfigSpec.BooleanValue GENERATE_VILLAGE_FARM_FD_CROPS;
-	public static ForgeConfigSpec.BooleanValue GENERATE_WILD_CABBAGES;
-	public static ForgeConfigSpec.IntValue CHANCE_WILD_CABBAGES;
-	public static ForgeConfigSpec.BooleanValue GENERATE_WILD_BEETROOTS;
-	public static ForgeConfigSpec.IntValue CHANCE_WILD_BEETROOTS;
-	public static ForgeConfigSpec.BooleanValue GENERATE_WILD_POTATOES;
-	public static ForgeConfigSpec.IntValue CHANCE_WILD_POTATOES;
-	public static ForgeConfigSpec.BooleanValue GENERATE_WILD_ONIONS;
-	public static ForgeConfigSpec.IntValue CHANCE_WILD_ONIONS;
-	public static ForgeConfigSpec.BooleanValue GENERATE_WILD_CARROTS;
-	public static ForgeConfigSpec.IntValue CHANCE_WILD_CARROTS;
-	public static ForgeConfigSpec.BooleanValue GENERATE_WILD_TOMATOES;
-	public static ForgeConfigSpec.IntValue CHANCE_WILD_TOMATOES;
-	public static ForgeConfigSpec.BooleanValue GENERATE_WILD_RICE;
-	public static ForgeConfigSpec.IntValue CHANCE_WILD_RICE;
-	public static ForgeConfigSpec.BooleanValue GENERATE_BROWN_MUSHROOM_COLONIES;
-	public static ForgeConfigSpec.IntValue CHANCE_BROWN_MUSHROOM_COLONIES;
-	public static ForgeConfigSpec.BooleanValue GENERATE_RED_MUSHROOM_COLONIES;
-	public static ForgeConfigSpec.IntValue CHANCE_RED_MUSHROOM_COLONIES;
 
 	// CLIENT
 	public static final String CATEGORY_CLIENT = "client";
@@ -137,56 +119,6 @@ public class Configuration
 				.define("genVillageCompostHeaps", true);
 		GENERATE_VILLAGE_FARM_FD_CROPS = COMMON_BUILDER.comment("Should FD crops show up planted randomly in various village farms?")
 				.define("genFDCropsOnVillageFarms", true);
-
-		COMMON_BUILDER.comment("Wild Cabbage generation").push("wild_cabbages");
-		CHANCE_WILD_CABBAGES = COMMON_BUILDER.comment("Chance of generating clusters. Smaller value = more frequent.")
-				.defineInRange("chance", 30, 0, Integer.MAX_VALUE);
-		COMMON_BUILDER.pop();
-
-		COMMON_BUILDER.comment("Sea Beet generation").push("wild_beetroots");
-		CHANCE_WILD_BEETROOTS = COMMON_BUILDER.comment("Chance of generating clusters. Smaller value = more frequent.")
-				.defineInRange("chance", 30, 0, Integer.MAX_VALUE);
-		COMMON_BUILDER.pop();
-
-		COMMON_BUILDER.comment("Wild Potato generation").push("wild_potatoes");
-		CHANCE_WILD_POTATOES = COMMON_BUILDER.comment("Chance of generating clusters. Smaller value = more frequent.")
-				.defineInRange("chance", 100, 0, Integer.MAX_VALUE);
-		COMMON_BUILDER.pop();
-
-		COMMON_BUILDER.comment("Wild Carrot generation").push("wild_carrots");
-		CHANCE_WILD_CARROTS = COMMON_BUILDER.comment("Chance of generating clusters. Smaller value = more frequent.")
-				.defineInRange("chance", 120, 0, Integer.MAX_VALUE);
-		COMMON_BUILDER.pop();
-
-		COMMON_BUILDER.comment("Wild Onion generation").push("wild_onions");
-		CHANCE_WILD_ONIONS = COMMON_BUILDER.comment("Chance of generating clusters. Smaller value = more frequent.")
-				.defineInRange("chance", 120, 0, Integer.MAX_VALUE);
-		COMMON_BUILDER.pop();
-
-		COMMON_BUILDER.comment("Tomato Vines generation").push("wild_tomatoes");
-		CHANCE_WILD_TOMATOES = COMMON_BUILDER.comment("Chance of generating clusters. Smaller value = more frequent.")
-				.defineInRange("chance", 100, 0, Integer.MAX_VALUE);
-		COMMON_BUILDER.pop();
-
-		COMMON_BUILDER.comment("Wild Rice generation").push("wild_rice");
-		CHANCE_WILD_RICE = COMMON_BUILDER.comment("Chance of generating clusters. Smaller value = more frequent.")
-				.defineInRange("chance", 20, 0, Integer.MAX_VALUE);
-		COMMON_BUILDER.pop();
-
-		COMMON_BUILDER.comment("Brown Mushroom Colony generation").push("brown_mushroom_colonies");
-		GENERATE_BROWN_MUSHROOM_COLONIES = COMMON_BUILDER.comment("Generate brown mushroom colonies on mushroom fields")
-				.define("genBrownMushroomColony", true);
-		CHANCE_BROWN_MUSHROOM_COLONIES = COMMON_BUILDER.comment("Chance of generating clusters. Smaller value = more frequent.")
-				.defineInRange("chance", 15, 0, Integer.MAX_VALUE);
-		COMMON_BUILDER.pop();
-
-		COMMON_BUILDER.comment("Red Mushroom Colony generation").push("red_mushroom_colonies");
-		GENERATE_RED_MUSHROOM_COLONIES = COMMON_BUILDER.comment("Generate red mushroom colonies on mushroom fields")
-				.define("genRedMushroomColony", true);
-		CHANCE_RED_MUSHROOM_COLONIES = COMMON_BUILDER.comment("Chance of generating clusters. Smaller value = more frequent.")
-				.defineInRange("chance", 15, 0, Integer.MAX_VALUE);
-		COMMON_BUILDER.pop();
-
 		COMMON_BUILDER.pop();
 
 		COMMON_CONFIG = COMMON_BUILDER.build();
