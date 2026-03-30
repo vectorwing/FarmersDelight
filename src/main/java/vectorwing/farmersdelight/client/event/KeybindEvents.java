5e4891776e2259a40a79716cf2cc35bff78ffce3
Lucas Barcellos <lucas.t.barcellos@gmail.com>
2026-03-30 13:20:40 -0300

    Fix KeybindEvents

--- src/main/java/vectorwing/farmersdelight/client/event/KeybindEvents.java
+++ src/main/java/vectorwing/farmersdelight/client/event/KeybindEvents.java
@@ -10,23 +10,23 @@ import net.neoforged.neoforge.client.event.ClientTickEvent;
 import net.neoforged.neoforge.network.PacketDistributor;
 import vectorwing.farmersdelight.FarmersDelight;
 import vectorwing.farmersdelight.common.item.SkilletItem;
-import vectorwing.farmersdelight.common.networking.ModNetworking;
+import vectorwing.farmersdelight.common.network.payload.FlipSkilletPayload;
 import vectorwing.farmersdelight.common.registry.ModDataComponents;
-import vectorwing.farmersdelight.common.registry.ModItems;
 
-@EventBusSubscriber(modid = FarmersDelight.MODID, bus = EventBusSubscriber.Bus.GAME, value = Dist.CLIENT)
-public class KeybindEvents {
-    @SubscribeEvent
-    public static void preClientTick(ClientTickEvent.Pre event) { // Run this on pre so inputs don't get eaten up.
-        Minecraft mc = Minecraft.getInstance();
-        Player player = mc.player;
-        if (player != null && player.isUsingItem()) {
-            ItemStack useItem = player.getUseItem();
-            if (useItem.getItem() instanceof SkilletItem && !useItem.has(ModDataComponents.SKILLET_FLIP_TIMESTAMP.get())) {
-                while (mc.options.keyAttack.consumeClick()) {
-                    PacketDistributor.sendToServer(ModNetworking.FlipSkilletMessage.INSTANCE);
-                }
-            }
-        }
-    }
+@EventBusSubscriber(modid = FarmersDelight.MODID, value = Dist.CLIENT)
+public class KeybindEvents
+{
+	@SubscribeEvent
+	public static void preClientTick(ClientTickEvent.Pre event) { // Run this on pre so inputs don't get eaten up.
+		Minecraft mc = Minecraft.getInstance();
+		Player player = mc.player;
+		if (player != null && player.isUsingItem()) {
+			ItemStack useItem = player.getUseItem();
+			if (useItem.getItem() instanceof SkilletItem && !useItem.has(ModDataComponents.SKILLET_FLIP_TIMESTAMP.get())) {
+				while (mc.options.keyAttack.consumeClick()) {
+					PacketDistributor.sendToServer(FlipSkilletPayload.INSTANCE);
+				}
+			}
+		}
+	}
 }
