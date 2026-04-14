coding-1ab/modpack-season-2

style: standardise indents to use tabs instead of spaces

mods/FarmersDelight/src/main/java/vectorwing/farmersdelight/common/block/entity/AbstractStoveBlockEntity.java
@@ -25,191 +25,191 @@ import java.util.Optional;
 import java.util.stream.IntStream;
 
 public abstract class AbstractStoveBlockEntity extends BlockEntity implements Clearable {
-    private final NonNullList<ItemStack> items;
-    private final RecipeType<? extends AbstractCookingRecipe> recipeType;
-    private final int[] cookingProgress;
-    private final int[] cookingTime;
-    private final RecipeManager.CachedCheck<Container, ? extends AbstractCookingRecipe> quickRecipeLookup;
+	private final NonNullList<ItemStack> items;
+	private final RecipeType<? extends AbstractCookingRecipe> recipeType;
+	private final int[] cookingProgress;
+	private final int[] cookingTime;
+	private final RecipeManager.CachedCheck<Container, ? extends AbstractCookingRecipe> quickRecipeLookup;
 
-    protected AbstractStoveBlockEntity(
-            BlockEntityType<?> blockEntityType,
-            BlockPos blockPos,
-            BlockState blockState,
-            RecipeType<? extends AbstractCookingRecipe> recipeType
-    ) {
-        super(blockEntityType, blockPos, blockState);
+	protected AbstractStoveBlockEntity(
+		BlockEntityType<?> blockEntityType,
+		BlockPos blockPos,
+		BlockState blockState,
+		RecipeType<? extends AbstractCookingRecipe> recipeType
+	) {
+		super(blockEntityType, blockPos, blockState);
 
-        int inventorySlotCount = this.getInventorySlotCount();
-        items = NonNullList.withSize(inventorySlotCount, ItemStack.EMPTY);
-        cookingProgress = new int[inventorySlotCount];
-        cookingTime = new int[inventorySlotCount];
-        this.recipeType = recipeType;
-        quickRecipeLookup = RecipeManager.createCheck(recipeType);
-    }
+		int inventorySlotCount = this.getInventorySlotCount();
+		items = NonNullList.withSize(inventorySlotCount, ItemStack.EMPTY);
+		cookingProgress = new int[inventorySlotCount];
+		cookingTime = new int[inventorySlotCount];
+		this.recipeType = recipeType;
+		quickRecipeLookup = RecipeManager.createCheck(recipeType);
+	}
 
-    protected abstract int getInventorySlotCount();
+	protected abstract int getInventorySlotCount();
 
-    public abstract Vec2 getStoveItemOffset(int index);
+	public abstract Vec2 getStoveItemOffset(int index);
 
-    public NonNullList<ItemStack> getItems() {
-        return this.items;
-    }
+	public NonNullList<ItemStack> getItems() {
+		return this.items;
+	}
 
-    @Override
-    public void load(CompoundTag tag) {
-        super.load(tag);
+	@Override
+	public void load(CompoundTag tag) {
+		super.load(tag);
 
-        this.items.clear();
-        ContainerHelper.loadAllItems(tag, this.items);
+		this.items.clear();
+		ContainerHelper.loadAllItems(tag, this.items);
 
-        if (tag.contains("CookingTimes", 11)) {
-            int[] arrayCookingTimes = tag.getIntArray("CookingTimes");
-            System.arraycopy(arrayCookingTimes, 0, this.cookingProgress, 0, Math.min(this.cookingTime.length, arrayCookingTimes.length));
-        }
+		if (tag.contains("CookingTimes", 11)) {
+			int[] arrayCookingTimes = tag.getIntArray("CookingTimes");
+			System.arraycopy(arrayCookingTimes, 0, this.cookingProgress, 0, Math.min(this.cookingTime.length, arrayCookingTimes.length));
+		}
 
-        if (tag.contains("CookingTotalTimes", 11)) {
-            int[] arrayCookingTimesTotal = tag.getIntArray("CookingTotalTimes");
-            System.arraycopy(arrayCookingTimesTotal, 0, this.cookingTime, 0, Math.min(this.cookingTime.length, arrayCookingTimesTotal.length));
-        }
-    }
+		if (tag.contains("CookingTotalTimes", 11)) {
+			int[] arrayCookingTimesTotal = tag.getIntArray("CookingTotalTimes");
+			System.arraycopy(arrayCookingTimesTotal, 0, this.cookingTime, 0, Math.min(this.cookingTime.length, arrayCookingTimesTotal.length));
+		}
+	}
 
-    @Override
-    public void saveAdditional(CompoundTag tag) {
-        super.saveAdditional(tag);
-        ContainerHelper.saveAllItems(tag, this.items, true);
-        tag.putIntArray("CookingTimes", this.cookingProgress);
-        tag.putIntArray("CookingTotalTimes", this.cookingTime);
-    }
+	@Override
+	public void saveAdditional(CompoundTag tag) {
+		super.saveAdditional(tag);
+		ContainerHelper.saveAllItems(tag, this.items, true);
+		tag.putIntArray("CookingTimes", this.cookingProgress);
+		tag.putIntArray("CookingTotalTimes", this.cookingTime);
+	}
 
-    @Override
-    public ClientboundBlockEntityDataPacket getUpdatePacket() {
-        return ClientboundBlockEntityDataPacket.create(this);
-    }
+	@Override
+	public ClientboundBlockEntityDataPacket getUpdatePacket() {
+		return ClientboundBlockEntityDataPacket.create(this);
+	}
 
-    @Override
-    public CompoundTag getUpdateTag() {
-        CompoundTag tag = super.getUpdateTag();
-        ContainerHelper.saveAllItems(tag, this.items, true);
-        return tag;
-    }
+	@Override
+	public CompoundTag getUpdateTag() {
+		CompoundTag tag = super.getUpdateTag();
+		ContainerHelper.saveAllItems(tag, this.items, true);
+		return tag;
+	}
 
-    public static void serverTick(Level level, BlockPos pos, BlockState state, AbstractStoveBlockEntity stoveEntity) {
-        if (stoveEntity.isEmpty()) return;
-        if (stoveEntity.shouldDropItems()) {
-            stoveEntity.dropAllItems();
-            stoveEntity.setChanged();
-            return;
-        }
+	public static void serverTick(Level level, BlockPos pos, BlockState state, AbstractStoveBlockEntity stoveEntity) {
+		if (stoveEntity.isEmpty()) return;
+		if (stoveEntity.shouldDropItems()) {
+			stoveEntity.dropAllItems();
+			stoveEntity.setChanged();
+			return;
+		}
 
-        if (state.getValue(AbstractStoveBlock.LIT)) {
-            stoveEntity.cookAndOutputItems();
-        } else {
-            stoveEntity.coolItems();
-        }
-    }
+		if (state.getValue(AbstractStoveBlock.LIT)) {
+			stoveEntity.cookAndOutputItems();
+		} else {
+			stoveEntity.coolItems();
+		}
+	}
 
-    private void cookAndOutputItems() {
-        assert this.level != null;
+	private void cookAndOutputItems() {
+		assert this.level != null;
 
-        boolean didChange = false;
-        for (int i = 0; i < this.items.size(); ++i) {
-            ItemStack ingredient = this.items.get(i);
-            if (ingredient.isEmpty()) continue;
-            didChange = true;
+		boolean didChange = false;
+		for (int i = 0; i < this.items.size(); ++i) {
+			ItemStack ingredient = this.items.get(i);
+			if (ingredient.isEmpty()) continue;
+			didChange = true;
 
-            ++cookingProgress[i];
-            if (cookingProgress[i] < cookingTime[i]) continue;
-            Container container = new SimpleContainer(ingredient);
-            ItemStack result = this.quickRecipeLookup.getRecipeFor(container, this.level).map(
-                (recipe) -> recipe.assemble(container, this.level.registryAccess())
-            ).orElse(ingredient);
+			++cookingProgress[i];
+			if (cookingProgress[i] < cookingTime[i]) continue;
+			Container container = new SimpleContainer(ingredient);
+			ItemStack result = this.quickRecipeLookup.getRecipeFor(container, this.level).map(
+				(recipe) -> recipe.assemble(container, this.level.registryAccess())
+			).orElse(ingredient);
 
-            if (!result.isItemEnabled(this.level.enabledFeatures())) continue;
-            Containers.dropItemStack(
-                this.level,
-                this.worldPosition.getX() + 0.5,
-                this.worldPosition.getY() + 1.0,
-                this.worldPosition.getZ() + 0.5,
-                result
-            );
-            this.items.set(i, ItemStack.EMPTY);
-            var state = this.getBlockState();
-            this.level.sendBlockUpdated(this.worldPosition, state, state, Block.UPDATE_ALL);
-            this.level.gameEvent(GameEvent.BLOCK_CHANGE, this.worldPosition, GameEvent.Context.of(state));
-        }
-        if (didChange) this.setChanged();
-    }
+			if (!result.isItemEnabled(this.level.enabledFeatures())) continue;
+			Containers.dropItemStack(
+				this.level,
+				this.worldPosition.getX() + 0.5,
+				this.worldPosition.getY() + 1.0,
+				this.worldPosition.getZ() + 0.5,
+				result
+			);
+			this.items.set(i, ItemStack.EMPTY);
+			var state = this.getBlockState();
+			this.level.sendBlockUpdated(this.worldPosition, state, state, Block.UPDATE_ALL);
+			this.level.gameEvent(GameEvent.BLOCK_CHANGE, this.worldPosition, GameEvent.Context.of(state));
+		}
+		if (didChange) this.setChanged();
+	}
 
-    private void coolItems() {
-        assert this.level != null;
+	private void coolItems() {
+		assert this.level != null;
 
-        boolean didChange = false;
-        for (int i = 0; i < this.items.size(); ++i) {
-            int thisItemCookingProgress =  this.cookingProgress[i];
-            if (thisItemCookingProgress <= 0) continue;
-            didChange = true;
-            this.cookingProgress[i] = Mth.clamp(thisItemCookingProgress - 2, 0, this.cookingTime[i]);
-        }
-        if (didChange) this.setChanged();
-    }
+		boolean didChange = false;
+		for (int i = 0; i < this.items.size(); ++i) {
+			int thisItemCookingProgress = this.cookingProgress[i];
+			if (thisItemCookingProgress <= 0) continue;
+			didChange = true;
+			this.cookingProgress[i] = Mth.clamp(thisItemCookingProgress - 2, 0, this.cookingTime[i]);
+		}
+		if (didChange) this.setChanged();
+	}
 
-    public Optional<? extends AbstractCookingRecipe> getCookingRecipe(ItemStack itemStack) {
-        assert this.level != null;
-        return this.quickRecipeLookup.getRecipeFor(new SimpleContainer(itemStack), this.level);
-    }
+	public Optional<? extends AbstractCookingRecipe> getCookingRecipe(ItemStack itemStack) {
+		assert this.level != null;
+		return this.quickRecipeLookup.getRecipeFor(new SimpleContainer(itemStack), this.level);
+	}
 
-    public int getNextEmptySlot() {
-        return IntStream.range(0, this.items.size())
-                .filter((i) -> this.items.get(i).isEmpty())
-                .findFirst()
-                .orElse(-1);
-    }
+	public int getNextEmptySlot() {
+		return IntStream.range(0, this.items.size())
+			.filter((i) -> this.items.get(i).isEmpty())
+			.findFirst()
+			.orElse(-1);
+	}
 
-    public boolean placeFood(@Nullable Entity entity, ItemStack foodStackToPlace, int foodCookingTime) {
-        assert this.level != null;
+	public boolean placeFood(@Nullable Entity entity, ItemStack foodStackToPlace, int foodCookingTime) {
+		assert this.level != null;
 
-        int emptySlotIndex = getNextEmptySlot();
-        if (emptySlotIndex < 0) return false;
-        assert this.items.get(emptySlotIndex).isEmpty();
+		int emptySlotIndex = getNextEmptySlot();
+		if (emptySlotIndex < 0) return false;
+		assert this.items.get(emptySlotIndex).isEmpty();
 
-        this.cookingTime[emptySlotIndex] = foodCookingTime;
-        this.cookingProgress[emptySlotIndex] = 0;
-        this.items.set(emptySlotIndex, foodStackToPlace.split(1));
-        var state = this.getBlockState();
-        this.level.sendBlockUpdated(this.worldPosition, state, state, Block.UPDATE_ALL);
-        this.level.gameEvent(GameEvent.BLOCK_CHANGE, this.worldPosition, GameEvent.Context.of(entity, state));
-        this.setChanged();
-        return true;
-    }
+		this.cookingTime[emptySlotIndex] = foodCookingTime;
+		this.cookingProgress[emptySlotIndex] = 0;
+		this.items.set(emptySlotIndex, foodStackToPlace.split(1));
+		var state = this.getBlockState();
+		this.level.sendBlockUpdated(this.worldPosition, state, state, Block.UPDATE_ALL);
+		this.level.gameEvent(GameEvent.BLOCK_CHANGE, this.worldPosition, GameEvent.Context.of(entity, state));
+		this.setChanged();
+		return true;
+	}
 
-    public boolean shouldDropItems() {
-        if (this.level == null) return false;
-        return AbstractStoveBlock.isStoveTopCovered(this.level, this.worldPosition, this.getBlockState());
-    }
+	public boolean shouldDropItems() {
+		if (this.level == null) return false;
+		return AbstractStoveBlock.isStoveTopCovered(this.level, this.worldPosition, this.getBlockState());
+	}
 
-    public boolean isEmpty() {
-        return this.items.stream().allMatch(ItemStack::isEmpty);
-    }
+	public boolean isEmpty() {
+		return this.items.stream().allMatch(ItemStack::isEmpty);
+	}
 
-    public boolean isFull() {
-        return this.items.stream().noneMatch(ItemStack::isEmpty);
-    }
+	public boolean isFull() {
+		return this.items.stream().noneMatch(ItemStack::isEmpty);
+	}
 
-    public void dropAllItems() {
-        if (this.level == null) return;
-        Containers.dropContents(this.level, this.worldPosition, items);
-        this.items.clear();
-        var state = this.getBlockState();
-        this.level.sendBlockUpdated(this.worldPosition, state, state, Block.UPDATE_ALL);
-        this.level.gameEvent(GameEvent.BLOCK_CHANGE, this.worldPosition, GameEvent.Context.of(state));
-    }
+	public void dropAllItems() {
+		if (this.level == null) return;
+		Containers.dropContents(this.level, this.worldPosition, items);
+		this.items.clear();
+		var state = this.getBlockState();
+		this.level.sendBlockUpdated(this.worldPosition, state, state, Block.UPDATE_ALL);
+		this.level.gameEvent(GameEvent.BLOCK_CHANGE, this.worldPosition, GameEvent.Context.of(state));
+	}
 
-    public void extinguish() {
-        if (this.level == null) return;
-        this.level.sendBlockUpdated(this.worldPosition, this.getBlockState(), this.getBlockState(), Block.UPDATE_ALL);
-        this.setChanged();
-        // level.gameEvent is called in AbstractStoveBlock::extinguish, so it is not called here
-    }
+	public void extinguish() {
+		if (this.level == null) return;
+		this.level.sendBlockUpdated(this.worldPosition, this.getBlockState(), this.getBlockState(), Block.UPDATE_ALL);
+		this.setChanged();
+		// level.gameEvent is called in AbstractStoveBlock::extinguish, so it is not called here
+	}
 
-    public void clearContent() { this.items.clear(); }
+	public void clearContent() { this.items.clear(); }
 }

vectorwing/FarmersDelight

src/main/java/vectorwing/farmersdelight/common/block/entity/AbstractStoveBlockEntity.java
@@ -24,192 +24,192 @@ import java.util.Optional;
 import java.util.stream.IntStream;
 
 public abstract class AbstractStoveBlockEntity extends BlockEntity implements Clearable {
-    private final NonNullList<ItemStack> items;
-    private final RecipeType<? extends AbstractCookingRecipe> recipeType;
-    private final int[] cookingProgress;
-    private final int[] cookingTime;
-    private final RecipeManager.CachedCheck<SingleRecipeInput, ? extends AbstractCookingRecipe> quickRecipeLookup;
+	private final NonNullList<ItemStack> items;
+	private final RecipeType<? extends AbstractCookingRecipe> recipeType;
+	private final int[] cookingProgress;
+	private final int[] cookingTime;
+	private final RecipeManager.CachedCheck<SingleRecipeInput, ? extends AbstractCookingRecipe> quickRecipeLookup;
 
-    protected AbstractStoveBlockEntity(
-            BlockEntityType<?> blockEntityType,
-            BlockPos blockPos,
-            BlockState blockState,
-            RecipeType<? extends AbstractCookingRecipe> recipeType
-    ) {
-        super(blockEntityType, blockPos, blockState);
+	protected AbstractStoveBlockEntity(
+		BlockEntityType<?> blockEntityType,
+		BlockPos blockPos,
+		BlockState blockState,
+		RecipeType<? extends AbstractCookingRecipe> recipeType
+	) {
+		super(blockEntityType, blockPos, blockState);
 
-        int inventorySlotCount = this.getInventorySlotCount();
-        items = NonNullList.withSize(inventorySlotCount, ItemStack.EMPTY);
-        cookingProgress = new int[inventorySlotCount];
-        cookingTime = new int[inventorySlotCount];
-        this.recipeType = recipeType;
-        quickRecipeLookup = RecipeManager.createCheck(recipeType);
-    }
+		int inventorySlotCount = this.getInventorySlotCount();
+		items = NonNullList.withSize(inventorySlotCount, ItemStack.EMPTY);
+		cookingProgress = new int[inventorySlotCount];
+		cookingTime = new int[inventorySlotCount];
+		this.recipeType = recipeType;
+		quickRecipeLookup = RecipeManager.createCheck(recipeType);
+	}
 
-    protected abstract int getInventorySlotCount();
+	protected abstract int getInventorySlotCount();
 
-    public abstract Vec2 getStoveItemOffset(int index);
+	public abstract Vec2 getStoveItemOffset(int index);
 
-    public NonNullList<ItemStack> getItems() {
-        return this.items;
-    }
+	public NonNullList<ItemStack> getItems() {
+		return this.items;
+	}
 
-    @Override
-    public void loadAdditional(CompoundTag tag, HolderLookup.Provider registries) {
-        super.loadAdditional(tag, registries);
+	@Override
+	public void loadAdditional(CompoundTag tag, HolderLookup.Provider registries) {
+		super.loadAdditional(tag, registries);
 
-        this.items.clear();
-        ContainerHelper.loadAllItems(tag, this.items, registries);
+		this.items.clear();
+		ContainerHelper.loadAllItems(tag, this.items, registries);
 
-        if (tag.contains("CookingTimes", 11)) {
-            int[] arrayCookingTimes = tag.getIntArray("CookingTimes");
-            System.arraycopy(arrayCookingTimes, 0, this.cookingProgress, 0, Math.min(this.cookingTime.length, arrayCookingTimes.length));
-        }
+		if (tag.contains("CookingTimes", 11)) {
+			int[] arrayCookingTimes = tag.getIntArray("CookingTimes");
+			System.arraycopy(arrayCookingTimes, 0, this.cookingProgress, 0, Math.min(this.cookingTime.length, arrayCookingTimes.length));
+		}
 
-        if (tag.contains("CookingTotalTimes", 11)) {
-            int[] arrayCookingTimesTotal = tag.getIntArray("CookingTotalTimes");
-            System.arraycopy(arrayCookingTimesTotal, 0, this.cookingTime, 0, Math.min(this.cookingTime.length, arrayCookingTimesTotal.length));
-        }
-    }
+		if (tag.contains("CookingTotalTimes", 11)) {
+			int[] arrayCookingTimesTotal = tag.getIntArray("CookingTotalTimes");
+			System.arraycopy(arrayCookingTimesTotal, 0, this.cookingTime, 0, Math.min(this.cookingTime.length, arrayCookingTimesTotal.length));
+		}
+	}
 
-    @Override
-    public void saveAdditional(CompoundTag tag, HolderLookup.Provider registries) {
-        super.saveAdditional(tag, registries);
-        ContainerHelper.saveAllItems(tag, this.items, true, registries);
-        tag.putIntArray("CookingTimes", this.cookingProgress);
-        tag.putIntArray("CookingTotalTimes", this.cookingTime);
-    }
+	@Override
+	public void saveAdditional(CompoundTag tag, HolderLookup.Provider registries) {
+		super.saveAdditional(tag, registries);
+		ContainerHelper.saveAllItems(tag, this.items, true, registries);
+		tag.putIntArray("CookingTimes", this.cookingProgress);
+		tag.putIntArray("CookingTotalTimes", this.cookingTime);
+	}
 
-    @Override
-    public ClientboundBlockEntityDataPacket getUpdatePacket() {
-        return ClientboundBlockEntityDataPacket.create(this);
-    }
+	@Override
+	public ClientboundBlockEntityDataPacket getUpdatePacket() {
+		return ClientboundBlockEntityDataPacket.create(this);
+	}
 
-    @Override
-    public CompoundTag getUpdateTag(HolderLookup.Provider registries) {
-        CompoundTag tag = super.getUpdateTag(registries);
-        ContainerHelper.saveAllItems(tag, this.items, true, registries);
-        return tag;
-    }
+	@Override
+	public CompoundTag getUpdateTag(HolderLookup.Provider registries) {
+		CompoundTag tag = super.getUpdateTag(registries);
+		ContainerHelper.saveAllItems(tag, this.items, true, registries);
+		return tag;
+	}
 
-    public static void serverTick(Level level, BlockPos pos, BlockState state, AbstractStoveBlockEntity stoveEntity) {
-        if (stoveEntity.isEmpty()) return;
-        if (stoveEntity.shouldDropItems()) {
-            stoveEntity.dropAllItems();
-            stoveEntity.setChanged();
-            return;
-        }
+	public static void serverTick(Level level, BlockPos pos, BlockState state, AbstractStoveBlockEntity stoveEntity) {
+		if (stoveEntity.isEmpty()) return;
+		if (stoveEntity.shouldDropItems()) {
+			stoveEntity.dropAllItems();
+			stoveEntity.setChanged();
+			return;
+		}
 
-        if (state.getValue(AbstractStoveBlock.LIT)) {
-            stoveEntity.cookAndOutputItems();
-        } else {
-            stoveEntity.coolItems();
-        }
-    }
+		if (state.getValue(AbstractStoveBlock.LIT)) {
+			stoveEntity.cookAndOutputItems();
+		} else {
+			stoveEntity.coolItems();
+		}
+	}
 
-    private void cookAndOutputItems() {
-        assert this.level != null;
+	private void cookAndOutputItems() {
+		assert this.level != null;
 
-        boolean didChange = false;
-        for (int i = 0; i < this.items.size(); ++i) {
-            ItemStack ingredient = this.items.get(i);
-            if (ingredient.isEmpty()) continue;
-            didChange = true;
+		boolean didChange = false;
+		for (int i = 0; i < this.items.size(); ++i) {
+			ItemStack ingredient = this.items.get(i);
+			if (ingredient.isEmpty()) continue;
+			didChange = true;
 
-            ++cookingProgress[i];
-            if (cookingProgress[i] < cookingTime[i]) continue;
+			++cookingProgress[i];
+			if (cookingProgress[i] < cookingTime[i]) continue;
 
-            var input = new SingleRecipeInput(ingredient);
-            ItemStack result = this.quickRecipeLookup.getRecipeFor(input, this.level)
-                .map((recipe) -> recipe.value().assemble(input, this.level.registryAccess()))
-                .orElse(ingredient);
+			var input = new SingleRecipeInput(ingredient);
+			ItemStack result = this.quickRecipeLookup.getRecipeFor(input, this.level)
+				.map((recipe) -> recipe.value().assemble(input, this.level.registryAccess()))
+				.orElse(ingredient);
 
-            if (!result.isItemEnabled(this.level.enabledFeatures())) continue;
-            Containers.dropItemStack(
-                this.level,
-                this.worldPosition.getX() + 0.5,
-                this.worldPosition.getY() + 1.0,
-                this.worldPosition.getZ() + 0.5,
-                result
-            );
-            this.items.set(i, ItemStack.EMPTY);
-            var state = this.getBlockState();
-            this.level.sendBlockUpdated(this.worldPosition, state, state, Block.UPDATE_ALL);
-            this.level.gameEvent(GameEvent.BLOCK_CHANGE, this.worldPosition, GameEvent.Context.of(state));
-        }
-        if (didChange) this.setChanged();
-    }
+			if (!result.isItemEnabled(this.level.enabledFeatures())) continue;
+			Containers.dropItemStack(
+				this.level,
+				this.worldPosition.getX() + 0.5,
+				this.worldPosition.getY() + 1.0,
+				this.worldPosition.getZ() + 0.5,
+				result
+			);
+			this.items.set(i, ItemStack.EMPTY);
+			var state = this.getBlockState();
+			this.level.sendBlockUpdated(this.worldPosition, state, state, Block.UPDATE_ALL);
+			this.level.gameEvent(GameEvent.BLOCK_CHANGE, this.worldPosition, GameEvent.Context.of(state));
+		}
+		if (didChange) this.setChanged();
+	}
 
-    private void coolItems() {
-        assert this.level != null;
+	private void coolItems() {
+		assert this.level != null;
 
-        boolean didChange = false;
-        for (int i = 0; i < this.items.size(); ++i) {
-            int thisItemCookingProgress =  this.cookingProgress[i];
-            if (thisItemCookingProgress <= 0) continue;
-            didChange = true;
-            this.cookingProgress[i] = Mth.clamp(thisItemCookingProgress - 2, 0, this.cookingTime[i]);
-        }
-        if (didChange) this.setChanged();
-    }
+		boolean didChange = false;
+		for (int i = 0; i < this.items.size(); ++i) {
+			int thisItemCookingProgress = this.cookingProgress[i];
+			if (thisItemCookingProgress <= 0) continue;
+			didChange = true;
+			this.cookingProgress[i] = Mth.clamp(thisItemCookingProgress - 2, 0, this.cookingTime[i]);
+		}
+		if (didChange) this.setChanged();
+	}
 
-    public Optional<? extends RecipeHolder<? extends AbstractCookingRecipe>> getCookingRecipe(ItemStack itemStack) {
-        assert this.level != null;
-        return this.quickRecipeLookup.getRecipeFor(new SingleRecipeInput(itemStack), this.level);
-    }
+	public Optional<? extends RecipeHolder<? extends AbstractCookingRecipe>> getCookingRecipe(ItemStack itemStack) {
+		assert this.level != null;
+		return this.quickRecipeLookup.getRecipeFor(new SingleRecipeInput(itemStack), this.level);
+	}
 
-    public int getNextEmptySlot() {
-        return IntStream.range(0, this.items.size())
-                .filter((i) -> this.items.get(i).isEmpty())
-                .findFirst()
-                .orElse(-1);
-    }
+	public int getNextEmptySlot() {
+		return IntStream.range(0, this.items.size())
+			.filter((i) -> this.items.get(i).isEmpty())
+			.findFirst()
+			.orElse(-1);
+	}
 
-    public boolean placeFood(@Nullable Entity entity, ItemStack foodStackToPlace, RecipeHolder<? extends AbstractCookingRecipe> recipe) {
-        assert this.level != null;
+	public boolean placeFood(@Nullable Entity entity, ItemStack foodStackToPlace, RecipeHolder<? extends AbstractCookingRecipe> recipe) {
+		assert this.level != null;
 
-        int emptySlotIndex = getNextEmptySlot();
-        if (emptySlotIndex < 0) return false;
-        assert this.items.get(emptySlotIndex).isEmpty();
+		int emptySlotIndex = getNextEmptySlot();
+		if (emptySlotIndex < 0) return false;
+		assert this.items.get(emptySlotIndex).isEmpty();
 
-        this.cookingTime[emptySlotIndex] = recipe.value().getCookingTime();
-        this.cookingProgress[emptySlotIndex] = 0;
-        this.items.set(emptySlotIndex, foodStackToPlace.split(1));
-        var state = this.getBlockState();
-        this.level.sendBlockUpdated(this.worldPosition, state, state, Block.UPDATE_ALL);
-        this.level.gameEvent(GameEvent.BLOCK_CHANGE, this.worldPosition, GameEvent.Context.of(entity, state));
-        this.setChanged();
-        return true;
-    }
+		this.cookingTime[emptySlotIndex] = recipe.value().getCookingTime();
+		this.cookingProgress[emptySlotIndex] = 0;
+		this.items.set(emptySlotIndex, foodStackToPlace.split(1));
+		var state = this.getBlockState();
+		this.level.sendBlockUpdated(this.worldPosition, state, state, Block.UPDATE_ALL);
+		this.level.gameEvent(GameEvent.BLOCK_CHANGE, this.worldPosition, GameEvent.Context.of(entity, state));
+		this.setChanged();
+		return true;
+	}
 
-    public boolean shouldDropItems() {
-        if (this.level == null) return false;
-        return AbstractStoveBlock.isStoveTopCovered(this.level, this.worldPosition, this.getBlockState());
-    }
+	public boolean shouldDropItems() {
+		if (this.level == null) return false;
+		return AbstractStoveBlock.isStoveTopCovered(this.level, this.worldPosition, this.getBlockState());
+	}
 
-    public boolean isEmpty() {
-        return this.items.stream().allMatch(ItemStack::isEmpty);
-    }
+	public boolean isEmpty() {
+		return this.items.stream().allMatch(ItemStack::isEmpty);
+	}
 
-    public boolean isFull() {
-        return this.items.stream().noneMatch(ItemStack::isEmpty);
-    }
+	public boolean isFull() {
+		return this.items.stream().noneMatch(ItemStack::isEmpty);
+	}
 
-    public void dropAllItems() {
-        if (this.level == null) return;
-        Containers.dropContents(this.level, this.worldPosition, items);
-        this.items.clear();
-        var state = this.getBlockState();
-        this.level.sendBlockUpdated(this.worldPosition, state, state, Block.UPDATE_ALL);
-        this.level.gameEvent(GameEvent.BLOCK_CHANGE, this.worldPosition, GameEvent.Context.of(state));
-    }
+	public void dropAllItems() {
+		if (this.level == null) return;
+		Containers.dropContents(this.level, this.worldPosition, items);
+		this.items.clear();
+		var state = this.getBlockState();
+		this.level.sendBlockUpdated(this.worldPosition, state, state, Block.UPDATE_ALL);
+		this.level.gameEvent(GameEvent.BLOCK_CHANGE, this.worldPosition, GameEvent.Context.of(state));
+	}
 
-    public void extinguish() {
-        if (this.level == null) return;
-        this.level.sendBlockUpdated(this.worldPosition, this.getBlockState(), this.getBlockState(), Block.UPDATE_ALL);
-        this.setChanged();
-        // level.gameEvent is called in AbstractStoveBlock::extinguish, so it is not called here
-    }
+	public void extinguish() {
+		if (this.level == null) return;
+		this.level.sendBlockUpdated(this.worldPosition, this.getBlockState(), this.getBlockState(), Block.UPDATE_ALL);
+		this.setChanged();
+		// level.gameEvent is called in AbstractStoveBlock::extinguish, so it is not called here
+	}
 
-    public void clearContent() { this.items.clear(); }
+	public void clearContent() { this.items.clear(); }
 }