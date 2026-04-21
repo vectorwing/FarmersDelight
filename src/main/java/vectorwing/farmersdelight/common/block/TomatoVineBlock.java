67df32ae84c958bd0b8141dccc4e49a572b45195
Lucas Barcellos <lucas.t.barcellos@gmail.com>
2026-04-21 17:45:31 -0300

    Add backwards-compat TomatoVineBlock

---
+++ src/main/java/vectorwing/farmersdelight/common/block/TomatoVineBlock.java
@@ -0,0 +1,14 @@
+package vectorwing.farmersdelight.common.block;
+
+// Renamed to TomatoBlock. This class will be removed in the next minor/major release.
+@Deprecated(forRemoval = true)
+public class TomatoVineBlock extends TomatoBlock
+{
+	public TomatoVineBlock(Properties properties) {
+		super(properties);
+	}
+
+	protected TomatoVineBlock(Properties properties, boolean dummy) {
+		super(properties, dummy);
+	}
+}
