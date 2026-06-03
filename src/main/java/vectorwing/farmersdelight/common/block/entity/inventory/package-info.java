f8b7ffbd60b04aaefe30ec69d13eda33a9947a3b
AyOhEe <74308087+AyOhEe@users.noreply.github.com>
2026-06-03 23:45:24 +0100

    Maybe fix package-info annotations

--- src/main/java/vectorwing/farmersdelight/common/block/entity/inventory/package-info.java
+++ src/main/java/vectorwing/farmersdelight/common/block/entity/inventory/package-info.java
@@ -2,6 +2,6 @@
 @MethodsReturnNonnullByDefault
 package vectorwing.farmersdelight.common.block.entity.inventory;
 
-import net.minecraft.MethodsReturnNonnullByDefault;
+import com.mojang.logging.annotations.MethodsReturnNonnullByDefault;
 
 import javax.annotation.ParametersAreNonnullByDefault;
