e871ed89a92a25a3db5c2db270cdf5aff0538a0c
AyOhEe <74308087+AyOhEe@users.noreply.github.com>
2026-06-04 13:29:20 +0100

    Fix farmland mixin

--- src/main/java/vectorwing/farmersdelight/common/mixin/KeepRichSoilUntrampledMixin.java
+++ src/main/java/vectorwing/farmersdelight/common/mixin/KeepRichSoilUntrampledMixin.java
@@ -3,7 +3,7 @@ package vectorwing.farmersdelight.common.mixin;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.FarmBlock;
+import net.minecraft.world.level.block.FarmlandBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,7 +11,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import vectorwing.farmersdelight.common.block.RichSoilFarmlandBlock;
 
-@Mixin(FarmBlock.class)
+@Mixin(FarmlandBlock.class)
 public class KeepRichSoilUntrampledMixin {
     @Inject(at = @At(value = "HEAD"), method = "turnToDirt", cancellable = true)
     private static void turnToDirt(Entity entity, BlockState state, Level level, BlockPos pos, CallbackInfo ci) {
