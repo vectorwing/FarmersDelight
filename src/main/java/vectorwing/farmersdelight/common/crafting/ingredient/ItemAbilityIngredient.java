72a99b5d1e44ba85ea72d709624888bcc2cc1292
AyOhEe <74308087+AyOhEe@users.noreply.github.com>
2026-06-07 18:25:27 +0100

    ItemAbilityIngredient

--- src/main/java/vectorwing/farmersdelight/common/crafting/ingredient/ItemAbilityIngredient.java
+++ src/main/java/vectorwing/farmersdelight/common/crafting/ingredient/ItemAbilityIngredient.java
@@ -3,9 +3,10 @@ package vectorwing.farmersdelight.common.crafting.ingredient;
 import com.mojang.logging.annotations.MethodsReturnNonnullByDefault;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.core.Holder;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.crafting.Ingredient;
 import net.neoforged.neoforge.common.ItemAbility;
 import net.neoforged.neoforge.common.crafting.ICustomIngredient;
 import net.neoforged.neoforge.common.crafting.IngredientType;
@@ -27,17 +28,17 @@ public class ItemAbilityIngredient implements ICustomIngredient
 			).apply(inst, ItemAbilityIngredient::new));
 
 	protected final ItemAbility itemAbility;
-	protected Stream<ItemStack> itemStacks;
+	protected Stream<Holder<Item>> itemHolders;
 
 	public ItemAbilityIngredient(ItemAbility itemAbility) {
 		this.itemAbility = itemAbility;
 	}
 
 	protected void dissolve() {
-		if (this.itemStacks == null) {
-			itemStacks = BuiltInRegistries.ITEM.stream()
-					.map(ItemStack::new)
-					.filter(stack -> stack.canPerformAction(itemAbility));
+		if (this.itemHolders == null) {
+			itemHolders = BuiltInRegistries.ITEM.stream()
+					.filter(item -> item.canPerformAction(new ItemStack(item), itemAbility))
+					.map(BuiltInRegistries.ITEM::wrapAsHolder);
 		}
 	}
 
@@ -47,9 +48,9 @@ public class ItemAbilityIngredient implements ICustomIngredient
 	}
 
 	@Override
-	public Stream<ItemStack> getItems() {
+	public Stream<Holder<Item>> items() {
 		dissolve();
-		return itemStacks;
+		return itemHolders;
 	}
 
 	@Override
