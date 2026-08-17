6aa9468ffb7161dce84556da9a5445710648304b
Lucas Barcellos <lucas.t.barcellos@gmail.com>
2026-08-16 21:19:01 -0300

    Fix Canvas Sign NBT loss on CTRL pick

--- changelog.md
+++ changelog.md
@@ -5,6 +5,7 @@
 ### Fixes
 - Fixed Rope reeling not working if the rope is touching the void layer (minimum build height);
 - Fixed suppressed packet error due to an unsafe property check in TomatoBlock;
+- Fixed Canvas Signs not keeping text color when NBT-copied (such as CTRL + middle-click);
 
 ## 1.3.2
 
--- src/main/java/vectorwing/farmersdelight/common/block/entity/CanvasSignBlockEntity.java
+++ src/main/java/vectorwing/farmersdelight/common/block/entity/CanvasSignBlockEntity.java
@@ -1,15 +1,20 @@
 package vectorwing.farmersdelight.common.block.entity;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.entity.SignBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import vectorwing.farmersdelight.common.block.state.CanvasSign;
 import vectorwing.farmersdelight.common.registry.ModBlockEntityTypes;
 
 public class CanvasSignBlockEntity extends SignBlockEntity
 {
 	public CanvasSignBlockEntity(BlockPos pos, BlockState state) {
 		super(pos, state);
+		if (state.getBlock() instanceof CanvasSign canvasSign && canvasSign.isDarkBackground()) {
+			this.frontText = createDefaultSignText().setColor(DyeColor.WHITE);
+		}
 	}
 
 	@Override
